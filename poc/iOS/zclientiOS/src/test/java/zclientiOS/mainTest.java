d68cf3764d18a82f82f863c2b990f7f06e37dc3e
jazzpetzah <piazadji@gmail.com>
2014-05-29 17:19:29 +0300

    progress

--- poc/iOS/zclientiOS/src/test/java/zclientiOS/mainTest.java
+++ poc/iOS/zclientiOS/src/test/java/zclientiOS/mainTest.java
@@ -29,15 +29,15 @@ public class mainTest
 {
 	 private static AppiumDriver driver;
 	 private String login = "piotr.iazadji@wearezeta.com";
-	 private String password ="asdfer123qwertywz";
+	 private String password ="asdfer123";
 	 private static String myUserName = "Piotr Iazadji";
 	 private String loginQA = "mengli.wong+mqa6@wearezeta.com";
 	 private String passwordQA ="123456";
 	 private static String qaUserName = "mqa6";
-	 private String conversation = "Sergey";
-//	 private String testMessage = "Test test\n";
-	 private String testMessage = UUID.randomUUID().toString() + "\n";
+	 private String conversation = "Maxim";
+	 private String testMessage = UUID.randomUUID().toString();
 	 private String bandle = "com.wearezeta.zclient-alpha";
+	 
 			 
 			 
 			 
@@ -45,16 +45,18 @@ public class mainTest
 	 @Before
 	    public void setUp() throws Exception 
 	    {
-	        String appPath = "/Users/admin/Desktop/ZClient_Debug_3f609be.ipa";
+	        String appPath = "/Project/ZClient.app";
 	        DesiredCapabilities capabilities = new DesiredCapabilities();
-	        //capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
 	        capabilities.setCapability("platformName", "iOS");
-	        //capabilities.setCapability("deviceName","Android Emulator");
-	        //capabilities.setCapability("platformVersion", "4.4");
 	        capabilities.setCapability("app", appPath);
+
+	        //capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+	        //capabilities.setCapability("deviceName","iPhone 4S");
+	        //capabilities.setCapability("platformVersion", "4.4");
 	        //capabilities.setCapability("app-package", "com.waz.zclient");
 	        //capabilities.setCapability("app-activity", ".RegistrationActivity");
-	        driver = new AppiumDriver(new URL("http://192.168.232.128:4723/wd/hub"), capabilities);
+	        
+	        driver = new AppiumDriver(new URL("http://192.168.153.128:4723/wd/hub"), capabilities);
 	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	    }
 	 
@@ -63,6 +65,24 @@ public class mainTest
 	 {
 		 Thread.sleep(3000);
 		 driver.quit();
+	 }
+	 
+	 
+	 public void LoginAs(String name, String pass)
+	 {
+		 WebElement LoginBtn = driver.findElementByName("SignIn");
+		 WebDriverWait wait = new WebDriverWait(driver, 30);
+		 wait.until(ExpectedConditions.elementToBeClickable(LoginBtn));
+		 LoginBtn.click();
+		 
+		 WebElement LoginInput = driver.findElementByName("SignInEmail");
+		 WebElement PassInput = driver.findElementByName("SignInPassword");
+		 WebElement SignInBtn = driver.findElementByName("ConfirmSignIn");
+		 
+		 wait.until(ExpectedConditions.elementToBeClickable(LoginInput));
+		 LoginInput.sendKeys(name);
+		 PassInput.sendKeys(pass);
+		 SignInBtn.click();
 	 }
 	 
 	 public static void ScrollToName(String name)
@@ -102,9 +122,12 @@ public class mainTest
 	 
 	 public void ChatLastMessageValidation(String text)
 	 {
-		 List<WebElement> chat = driver.findElementsByClassName("UIATextView");
-		 String lastMessage = chat.get(chat.size()-1).getAttribute("name");
-		 Assert.assertEquals(lastMessage, text);
+		 List<WebElement> chat = driver.findElementsByClassName("UIATableCell");
+		 int pos = chat.size();
+		 String lastMessageLoc = "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[" + pos + "]/UIATextView[1]";
+		 WebElement lastMessageEl = driver.findElementByXPath(lastMessageLoc);
+		 String MessageBody = lastMessageEl.getAttribute("name");
+		 Assert.assertEquals(text, MessageBody);
 	 }
 	 
 	 public void iPhoneSwipeRight() {
@@ -123,41 +146,82 @@ public class mainTest
 		 driver.swipe(170, 200, 170, 400, 500);
 	}
 	 
+	 public void TapMainScreen()
+	 {
+		 WebElement tableView = driver.findElementByClassName("UIATableView");
+		 tableView.click();
+	 }
+	 
 	 public void WaitElementClickable(WebElement element) {
 		 WebDriverWait wait = new WebDriverWait(driver, 30);
 		 wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
 	 
+	 public void SendMessage(String message)
+	 {
+		 WebElement MessageLabel = driver.findElementByName("TAP OR SLIDE  ");
+		 WebDriverWait wait = new WebDriverWait(driver, 30);
+		 wait.until(ExpectedConditions.elementToBeClickable(MessageLabel));
+		 MessageLabel.click();
+		 WebElement MessageInput = driver.findElementByName("ComposeControllerTextView");
+		 MessageInput.sendKeys(message + "\n");
+	 }
+	 
+	 public void MessageValidation(String message)
+	 {
+		 WebElement sentMessage = null;
+		 try
+		 {
+			 sentMessage = driver.findElementByName(message);
+		 }
+		 catch(Exception e){//nothing
+		 }
+		 Assert.assertNotNull("Element not found", sentMessage);
+	 }
+	 
+	 public void DismissAlert()
+	 {
+		 
+		 WebElement Alert = driver.findElementByClassName("UIAAlert");
+		 driver.switchTo().alert().dismiss();
+		 //WebDriverWait wait = new WebDriverWait(driver, 30);
+		 //wait.until(ExpectedConditions.elementToBeClickable(Alert));
+		 //WebElement DismissBtn = driver.findElementByName("Don’t Allow");
+		 //DismissBtn.click();
+	 }
+	 
+	 public void AcceptAlert()
+	 {
+		 
+		 WebElement Alert = driver.findElementByClassName("UIAAlert");
+		 driver.switchTo().alert().accept();
+	 }
+	 
+	 public void PickUser(String username)
+	 {
+		 WebElement ZLogoImg = driver.findElementByName("PULL TO START CONVERSATION");
+		 iPhoneSwipeDown();
+		 WebElement SearchBox = driver.findElementByName("textViewSearch");
+		 SearchBox.click();
+		 SearchBox.sendKeys(username);
+		 WebElement user = driver.findElementByXPath("//UIAApplication[1]/UIAWindow[1]/UIACollectionView[2]/UIACollectionCell[1]/UIAStaticText[1]");
+		 user.click();
+	 }
+	 
+	 public void PickUserAndStartConversation(String username)
+	 {
+		 PickUser(username);
+		 WebElement CreateConversationBtn = driver.findElementByName("CREATE CONVERSATION");
+		 CreateConversationBtn.click();
+	 }
+	 
 	 
 	 @Test
 	 public void LoginLogoutTest()
 	 {
-		 WebElement LoginBtn = driver.findElementByName("SignIn");
+		 LoginAs(login, password);
 		 
-		 WebDriverWait wait = new WebDriverWait(driver, 30);
-		 wait.until(ExpectedConditions.elementToBeClickable(LoginBtn));
-		 LoginBtn.click();
-		 
-		 WebElement LoginInput = driver.findElementByName("SignInEmail");
-		 WebElement PassInput = driver.findElementByName("SignInPassword");
-		 WebElement SignInBtn = driver.findElementByName("ConfirmSignIn");
-		 
-		 wait.until(ExpectedConditions.elementToBeClickable(LoginInput));
-		 LoginInput.sendKeys(login);
-		 PassInput.sendKeys(password);
-		 SignInBtn.click();
-		 
-		 try
-		 {
-			 WebElement Alert = driver.findElementByXPath("//UIAApplication[1]/UIAWindow[4]/UIAAlert[1]/UIATableView[1]/UIATableCell[1]");
-			 wait.until(ExpectedConditions.elementToBeClickable(Alert));
-			 WebElement DismissBtn = driver.findElementByName("Don’t Allow");
-			 DismissBtn.click();
-		 }
-		 catch(Exception ex)
-		 {
-			 //do nothing
-		 }
+		 DismissAlert();
 		 
 		 WebElement myuserLabel = driver.findElementByName(myUserName);
 		 myuserLabel.click();
@@ -165,12 +229,14 @@ public class mainTest
 		 WebElement ContinueBtn = driver.findElementByName("CONTINUE");
 		 iPhoneSwipeLeft();
 		 
+		 
 		 iPhoneSwipeUp();
+		 
 		 WebElement SignOutBtn = driver.findElementByName("Sign out");
-		 wait.until(ExpectedConditions.elementToBeClickable(SignOutBtn));
+		 //wait.until(ExpectedConditions.elementToBeClickable(SignOutBtn));
 		 SignOutBtn.click();
 		 
-		 LoginBtn = null;
+		 WebElement LoginBtn = null;
 		 LoginBtn = driver.findElementByName("SignIn");
 		 Assert.assertNotNull(LoginBtn);
 		 
@@ -179,53 +245,15 @@ public class mainTest
 	 @Test
 	 public void SendMessageTest()
 	 {
-		 WebElement LoginBtn = driver.findElementByName("SignIn");
+		 LoginAs(login, password);
 		 
-		 WebDriverWait wait = new WebDriverWait(driver, 30);
-		 wait.until(ExpectedConditions.elementToBeClickable(LoginBtn));
-		 LoginBtn.click();
+		 DismissAlert();
 		 
-		 WebElement LoginInput = driver.findElementByName("SignInEmail");
-		 WebElement PassInput = driver.findElementByName("SignInPassword");
-		 WebElement SignInBtn = driver.findElementByName("ConfirmSignIn");
-		 
-		 wait.until(ExpectedConditions.elementToBeClickable(LoginInput));
-		 LoginInput.sendKeys(login);
-		 PassInput.sendKeys(password);
-		 SignInBtn.click();
-		 
-		 try
-		 {
-			 WebElement Alert = driver.findElementByXPath("//UIAApplication[1]/UIAWindow[4]/UIAAlert[1]/UIATableView[1]/UIATableCell[1]");
-			 wait.until(ExpectedConditions.elementToBeClickable(Alert));
-			 WebElement DismissBtn = driver.findElementByName("Don’t Allow");
-			 DismissBtn.click();
-		 }
-		 catch(Exception ex)
-		 {
-			 //do nothing
-		 }
-		 try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
 		 SrollToElementAndClick(conversation, "");
 		 
-		 WebElement MessageLabel = driver.findElementByName("TAP OR SLIDE  ");
-		 wait.until(ExpectedConditions.elementToBeClickable(MessageLabel));
-		 MessageLabel.click();
-		 WebElement MessageInput = driver.findElementByName("ComposeControllerTextView");
-		 MessageInput.sendKeys(testMessage);
-		 try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		 SendMessage(testMessage);
 		 
-		 //ChatLastMessageValidation(testMessage);
+		 ChatLastMessageValidation(testMessage);
 		 
 		 iPhoneSwipeRight();
 		 WebElement myuserLabel = driver.findElementByName(myUserName);
@@ -237,7 +265,7 @@ public class mainTest
 		 iPhoneSwipeUp();
 		 
 		 WebElement SignOutBtn = driver.findElementByName("Sign out");
-		 wait.until(ExpectedConditions.elementToBeClickable(SignOutBtn));
+		 //wait.until(ExpectedConditions.elementToBeClickable(SignOutBtn));
 		 SignOutBtn.click();
 		
 	 }
@@ -245,32 +273,9 @@ public class mainTest
 	 @Test
 	 public void SetNewPhotoTest()
 	 {
-		 WebElement LoginBtn = driver.findElementByName("SignIn");
+		 LoginAs(login, password);
 		 
-		 WebDriverWait wait = new WebDriverWait(driver, 30);
-		 wait.until(ExpectedConditions.elementToBeClickable(LoginBtn));
-		 LoginBtn.click();
-		 
-		 WebElement LoginInput = driver.findElementByName("SignInEmail");
-		 WebElement PassInput = driver.findElementByName("SignInPassword");
-		 WebElement SignInBtn = driver.findElementByName("ConfirmSignIn");
-		 
-		 wait.until(ExpectedConditions.elementToBeClickable(LoginInput));
-		 LoginInput.sendKeys(login);
-		 PassInput.sendKeys(password);
-		 SignInBtn.click();
-		 
-		 try
-		 {
-			 WebElement Alert = driver.findElementByXPath("//UIAApplication[1]/UIAWindow[4]/UIAAlert[1]/UIATableView[1]/UIATableCell[1]");
-			 wait.until(ExpectedConditions.elementToBeClickable(Alert));
-			 WebElement DismissBtn = driver.findElementByName("Don’t Allow");
-			 DismissBtn.click();
-		 }
-		 catch(Exception ex)
-		 {
-			 //do nothing
-		 }
+		 DismissAlert();
 		 
 		 WebElement myuserLabel = driver.findElementByName(myUserName);
 		 myuserLabel.click();
@@ -285,6 +290,24 @@ public class mainTest
 		 WebElement CameraBtn = driver.findElementByName("cameraButton");
 		 WaitElementClickable(CameraBtn);
 		 CameraBtn.click();
+		 
+	 }
+	 
+	 @Test
+	 public void UserPickerRealDevice()
+	 {
+		 LoginAs(login, password);
+		 
+		 DismissAlert();
+		
+		 PickUserAndStartConversation(conversation);		 
+		 
+		 SendMessage(testMessage);
+		 
+		 TapMainScreen();
+		 
+		 MessageValidation(testMessage);
+		 
 		 
 	 }
 	 
