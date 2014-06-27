94113ab2c35dbf49ff67ddc566e07fa21bf20d4c
Kirill Aleksandrov <Kyrylo.Aleksandrov@wearezeta.com>
2014-06-27 09:59:08 +0300

    Added support for findElementByClassName in Appium for MAC

--- appium-for-mac/AppiumForMac/Server/Controller/AfMElementLocator.m
+++ appium-for-mac/AppiumForMac/Server/Controller/AfMElementLocator.m
@@ -93,6 +93,47 @@
 		return matchedElement;
 	}
 
+    if (self.strategy == AppiumMacLocatoryStrategyClassName)
+    {
+        AfMElementLocator *windowLocator = [AfMElementLocator locatorWithSession:self.session using:@"xpath" value:@"//AXWindow"];
+        
+        PFUIElement *matchedElement = nil;
+        if (windowLocator != nil) {
+            NSMutableArray *windows = [NSMutableArray new];
+            [windowLocator findAllUsingBaseElement:nil results:windows statusCode:statusCode];
+            if (windows.count > 0) {
+                PFUIElement *mainWindow = [windows objectAtIndex:0];
+                int startX = mainWindow.AXPosition.pointValue.x;
+                int startY = mainWindow.AXPosition.pointValue.y;
+                int endX = startX + mainWindow.AXSize.pointValue.x;
+                int endY = startY + mainWindow.AXSize.pointValue.y;
+
+                for (int i = startX; i < endX; i+=20) {
+                    for (int j = startY; j < endY; j+=20) {
+                        NSError *error = nil;
+                        PFUIElement *newElement = [PFUIElement elementAtPoint:NSMakePoint(i, j) withDelegate:nil error:&error];
+                        if ([self.value caseInsensitiveCompare:newElement.AXRole] == NSOrderedSame) {
+                            matchedElement = newElement;
+                            
+                            *statusCode = kAfMStatusCodeSuccess;
+                            return matchedElement;
+                        }
+                    }
+                }
+            } else {
+                *statusCode = kAfMStatusCodeNoSuchWindow;
+                return nil;
+            }
+            if (matchedElement == nil) {
+                *statusCode = kAfMStatusCodeNoSuchElement;
+                return nil;
+            }
+            *statusCode = kAfMStatusCodeUnknownError;
+            return nil;
+        }
+        
+    }
+    
     // check if this the element we are looking for
     if ([self matchesElement:baseElement])
     {
