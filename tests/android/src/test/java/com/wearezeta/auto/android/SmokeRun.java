7573757f6ab4f7adc239124158ab58f73aec4450
Volodymyr Bezhenar <volodymyr.bezhenar@wearezeta.com>
2014-07-07 17:28:46 +0300

    Updated branch of tests to run

--- tests/android/src/test/java/com/wearezeta/auto/android/SmokeRun.java
+++ tests/android/src/test/java/com/wearezeta/auto/android/SmokeRun.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"html:target/report", "json:target/report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags  = { "@smoke" } )
+@CucumberOptions(format = {"html:target/report", "json:target/report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags  = { "@turun" } )
 public class SmokeRun {
 
 }
