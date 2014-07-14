d39f43c03f283b1e1e5bde5b56197d0072a2d67c
Volodymyr Bezhenar <volodymyr.bezhenar@wearezeta.com>
2014-07-14 15:49:18 +0300

    Removed @torun tag

--- tests/android/src/test/java/com/wearezeta/auto/android/DevRun.java
+++ tests/android/src/test/java/com/wearezeta/auto/android/DevRun.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {/*"com.wearezeta.auto.common.ZetaFormatter"*/"html:target/report", "json:target/report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags = {"@mute", "@torun"})
+@CucumberOptions(format = {/*"com.wearezeta.auto.common.ZetaFormatter"*/"html:target/report", "json:target/report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags = { "@mute" })
 public class DevRun {
 
 }
