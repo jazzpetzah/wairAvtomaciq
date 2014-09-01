da00d2f54e2324fe23f770e6f72c9ee43b4b334e
Volodymyr Bezhenar <volodymyr.bezhenar@wearezeta.com>
2014-09-01 13:19:53 +0300

    Updated json report name

--- tests/android/src/test/java/com/wearezeta/auto/android/StagingRun.java
+++ tests/android/src/test/java/com/wearezeta/auto/android/StagingRun.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"html:target/report", "json:target/report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags  = { "@staging" } )
+@CucumberOptions(format = {"html:target/report", "json:target/staging_report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags  = { "@staging" } )
 public class StagingRun {
 
 }
