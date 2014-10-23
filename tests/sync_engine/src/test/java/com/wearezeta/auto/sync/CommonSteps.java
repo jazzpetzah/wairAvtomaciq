7470d7a3c23a1cff27436cb668652c6d5926b0f7
Kirill Aleksandrov <Kyrylo.Aleksandrov@wearezeta.com>
2014-10-23 17:39:59 +0300

    Reordered sync engine test to improve performance

--- tests/sync_engine/src/test/java/com/wearezeta/auto/sync/CommonSteps.java
+++ tests/sync_engine/src/test/java/com/wearezeta/auto/sync/CommonSteps.java
@@ -492,22 +492,7 @@ public class CommonSteps {
 		}
 		storeIosPageSource(false);
 		storeOsxPageSource();
-		
-		//fast sending of messages from OSX
-		if (ExecutionContext.isOsxEnabled() && ExecutionContext.osxZeta().getState() != InstanceState.ERROR_CRASHED) {
-			for (int i = 0; i < ExecutionContext.osxZeta().getMessagesToSend(); i++) {
-				long startDate = new Date().getTime();
-				final String message = CommonUtils.generateGUID();
-				ExecutionContext.osxZeta().sender()
-						.sendTextMessage(SyncEngineUtil.CHAT_NAME, message, false);
-				long endDate = new Date().getTime();
-				log.debug("Time consumed for sending text message #" + i + " from OSX: "
-						+ (endDate - startDate) + "ms");
-			}
-		}
-		storeIosPageSource(false);
-		storeOsxPageSource();
-		
+
 		//send android, receive ios and osx
 		if (ExecutionContext.isAndroidEnabled() && ExecutionContext.androidZeta().getState() != InstanceState.ERROR_CRASHED) {
 			for (int i = 0; i < ExecutionContext.androidZeta().getMessagesToSend(); i++) {
@@ -551,7 +536,22 @@ public class CommonSteps {
 		}
 		storeIosPageSource(false);
 		storeOsxPageSource();
-		
+
+		//fast sending of messages from OSX
+		if (ExecutionContext.isOsxEnabled() && ExecutionContext.osxZeta().getState() != InstanceState.ERROR_CRASHED) {
+			for (int i = 0; i < ExecutionContext.osxZeta().getMessagesToSend(); i++) {
+				long startDate = new Date().getTime();
+				final String message = CommonUtils.generateGUID();
+				ExecutionContext.osxZeta().sender()
+						.sendTextMessage(SyncEngineUtil.CHAT_NAME, message, false);
+				long endDate = new Date().getTime();
+				log.debug("Time consumed for sending text message #" + i + " from OSX: "
+						+ (endDate - startDate) + "ms");
+			}
+		}
+		storeIosPageSource(false);
+		storeOsxPageSource();
+
 		//fast sending of messages from Android
 		if (ExecutionContext.isAndroidEnabled() && ExecutionContext.androidZeta().getState() != InstanceState.ERROR_CRASHED) {
 			for (int i = 0; i < ExecutionContext.androidZeta()
@@ -562,56 +562,6 @@ public class CommonSteps {
 						.sendTextMessage(SyncEngineUtil.CHAT_NAME, message, false);
 				long endDate = new Date().getTime();
 				log.debug("Time consumed for sending text message #" + i + " from Android: "
-						+ (endDate - startDate) + "ms");
-			}
-		}
-		storeIosPageSource(true);
-		storeOsxPageSource();
-	}
-	
-	@Given("I run fast sync engine test")
-	public void IRunFastSyncEngineTest() throws Exception {
-		// start sending message
-		if (ExecutionContext.isIosEnabled() && ExecutionContext.iosZeta().getState() != InstanceState.ERROR_CRASHED) {
-			com.wearezeta.auto.ios.pages.DialogPage page = com.wearezeta.auto.ios.pages.PagesCollection.dialogPage;
-			final String messages[] = new String[ExecutionContext.iosZeta().getMessagesToSend()];
-			for (int j = 0; j < messages.length; j++) {
-				messages[j] = CommonUtils.generateGUID();
-			}
-			long startDate = new Date().getTime();
-			ExecutionContext.iosZeta().sender()
-					.sendAllMessagesIos(messages, false);
-			long endDate = new Date().getTime();
-			log.debug("Time consumed for sending all text messages on ios: "
-				+ (endDate - startDate) + "ms");
-			
-		}
-		storeIosPageSource(false);
-		storeOsxPageSource();
-		
-		if (ExecutionContext.isOsxEnabled() && ExecutionContext.osxZeta().getState() != InstanceState.ERROR_CRASHED) {
-			for (int i = 0; i < ExecutionContext.osxZeta().getMessagesToSend(); i++) {
-				long startDate = new Date().getTime();
-				final String message = CommonUtils.generateGUID();
-				ExecutionContext.osxZeta().sender()
-						.sendTextMessage(SyncEngineUtil.CHAT_NAME, message, false);
-				long endDate = new Date().getTime();
-				log.debug("Time consumed for sending text message on osx: "
-						+ (endDate - startDate) + "ms");
-			}
-		}
-		storeIosPageSource(false);
-		storeOsxPageSource();
-		
-		if (ExecutionContext.isAndroidEnabled() && ExecutionContext.androidZeta().getState() != InstanceState.ERROR_CRASHED) {
-			for (int i = 0; i < ExecutionContext.androidZeta()
-					.getMessagesToSend(); i++) {
-				long startDate = new Date().getTime();
-				final String message = CommonUtils.generateGUID();
-				ExecutionContext.androidZeta().sender()
-						.sendTextMessage(SyncEngineUtil.CHAT_NAME, message, false);
-				long endDate = new Date().getTime();
-				log.debug("Time consumed for sending text message on android: "
 						+ (endDate - startDate) + "ms");
 			}
 		}
