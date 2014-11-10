80e3b8d4ef1bf775c5ec78d6ad77c4bd4d1082ae
Kirill Aleksandrov <Kyrylo.Aleksandrov@wearezeta.com>
2014-11-10 13:08:54 +0200

    Fixed locator of ADD TO CONVERSATION button

--- tests/macosx/src/main/java/com/wearezeta/auto/osx/locators/OSXLocators.java
+++ tests/macosx/src/main/java/com/wearezeta/auto/osx/locators/OSXLocators.java
@@ -79,7 +79,7 @@ public final class OSXLocators {
 	public static final String idAddPeopleButtonSingleChat = "userProfileViewButtonOnTheLeft";
 	public static final String idBlockUserButtonSingleChat = "userProfileViewButtonOnTheRight";
 	//OLD: public static final String idPeoplePickerAddToConversationButton = "people_picker_confirm_button";
-	public static final String idPeoplePickerAddToConversationButton = "ConfirmationBarViewConfirmButton";
+	public static final String namePeoplePickerAddToConversationButton = "ADD TO CONVERSATION";
 	public static final String idLeaveConversationButton = "leaveButton";
 	public static final String idRemoveUserFromConversation = "userProfileViewButtonOnTheRight";
 	public static final String idConversationScrollArea = "ConversationScrollArea";
