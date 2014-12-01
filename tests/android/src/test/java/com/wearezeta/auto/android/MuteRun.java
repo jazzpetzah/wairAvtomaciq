7cf85bdce84a6c8027f0c9750974957baa70d470
Sergey Hizhnyak <sergeii.khyzhniak@wearezeta.com>
2014-12-01 14:30:42 +0200

    separate runners for muted tests

--- tests/android/src/test/java/com/wearezeta/auto/android/MuteRun.java
+++ tests/android/src/test/java/com/wearezeta/auto/android/MuteRun.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"html:target/report", "json:target/staging_report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags  = { "@bug" } )
-public class BugRun {
+@CucumberOptions(format = {"html:target/report", "json:target/staging_report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags  = { "@mute" } )
+public class MuteRun {
 
 }
--- tests/ios/src/test/java/com/wearezeta/auto/ios/MuteRun.java
+++ tests/ios/src/test/java/com/wearezeta/auto/ios/MuteRun.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"html:target/report", "json:target/report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags  = { "@bug" } )
-public class BugRun {
+@CucumberOptions(format = {"html:target/report", "json:target/report.json", "com.wearezeta.auto.common.ZetaFormatter"}, tags  = { "@mute" } )
+public class MuteRun {
 
 }
