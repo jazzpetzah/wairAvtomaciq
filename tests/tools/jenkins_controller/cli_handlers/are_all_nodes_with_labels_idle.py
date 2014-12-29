396bc17ea8caf713cfb71ff27d5b4c6f7427b3ae
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2014-12-29 20:13:55 +0100

    Added new command to monitor Jenkins nodes

---
+++ tests/tools/jenkins_controller/cli_handlers/are_all_nodes_with_labels_idle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import xml.etree.ElementTree as ET
+
+from cli_handlers.cli_handler_base import CliHandlerBase
+
+
+class NoMatchesError(Exception):
+    pass
+
+
+class AreAllNodesWithLabelsIdle(CliHandlerBase):
+    def _build_options(self, parser):
+        super(AreAllNodesWithLabelsIdle, self)._build_options(parser)
+        parser.add_argument('--labels', required=True,
+                            help='List of comma-separated node labels to match')
+    
+    def _normalize_labels(self, labels_list):
+        normalized_labels = map(lambda x: x.strip(), labels_list)
+        return set(normalized_labels)
+    
+    def __call__(self):
+        parser = self._get_parser()
+        args = parser.parse_args()
+        expected_labels = self._normalize_labels(args.labels.split(','))
+        online_nodes_count = 0
+        for _, node in self._jenkins.get_nodes().iteritems():
+            if node.is_online():
+                response = node.jenkins.requester.get_and_confirm_status(
+                                                "%(baseurl)s/config.xml" % node.__dict__)
+                et = ET.fromstring(response.text)
+                node_labels_str = et.find('label').text
+                if node_labels_str:
+                    node_labels = self._normalize_labels(node_labels_str.split(' '))
+                else:
+                    node_labels = set()
+                if expected_labels.issubset(node_labels):
+                    online_nodes_count += 1
+                    if not node.is_idle():
+                        return False
+        if online_nodes_count == 0:
+            raise NoMatchesError('There are no online nodes with label(s) {0}'.\
+                                 format(args.labels))
+        return True
