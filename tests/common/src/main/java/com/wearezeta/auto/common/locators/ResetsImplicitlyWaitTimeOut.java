0d494dbaa9e7b126ed992f3f92e8b8df33212ea5
Maksym Kuvshynov <maksym.kuvshynov@wearezeta.com>
2015-01-16 16:32:46 +0200

    Appium update

---
+++ tests/common/src/main/java/com/wearezeta/auto/common/locators/ResetsImplicitlyWaitTimeOut.java
@@ -0,0 +1,7 @@
+package com.wearezeta.auto.common.locators;
+
+import java.util.concurrent.TimeUnit;
+
+interface ResetsImplicitlyWaitTimeOut {
+	void resetImplicitlyWaitTimeOut(long timeOut, TimeUnit timeUnit);
+}
