595af6c8d1b2afa48f38e51877f7fb0b45edd0c2
Maksym Kuvshynov <maksym.kuvshynov@wearezeta.com>
2015-01-20 10:19:24 +0200

    Zeta How

---
+++ tests/common/src/main/java/com/wearezeta/auto/common/locators/ZetaHow.java
@@ -0,0 +1,13 @@
+package com.wearezeta.auto.common.locators;
+
+public enum ZetaHow {
+	  CLASS_NAME,
+	  CSS,
+	  ID,
+	  LINK_TEXT,
+	  NAME,
+	  PARTIAL_LINK_TEXT,
+	  TAG_NAME,
+	  XPATH,
+	  UIAUTOMATOR
+	}
