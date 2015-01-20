f64d2fe6255817ad60b1e1a554ced640c6fe8af7
Maksym Kuvshynov <maksym.kuvshynov@wearezeta.com>
2015-01-20 11:13:38 +0200

    remove old file

--- tests/common/src/main/java/com/wearezeta/auto/common/locators/ResetsImplicitlyWaitTimeOut.java
+++
@@ -1,7 +0,0 @@
-package com.wearezeta.auto.common.locators;
-
-import java.util.concurrent.TimeUnit;
-
-interface ResetsImplicitlyWaitTimeOut {
-	void resetImplicitlyWaitTimeOut(long timeOut, TimeUnit timeUnit);
-}
