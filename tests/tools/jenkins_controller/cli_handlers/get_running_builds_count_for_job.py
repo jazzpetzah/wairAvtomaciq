9759286aa95f588547d6e83ae7141f2859461206
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-01-23 20:42:51 +0100

    Added new command to Jenkins controller tool

---
+++ tests/tools/jenkins_controller/cli_handlers/get_running_builds_count_for_job.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import traceback
+
+from cli_handlers.cli_handler_base import CliHandlerBase
+
+
+class GetRunningBuildsCountForJob(CliHandlerBase):
+    def _build_options(self, parser):
+        super(GetRunningBuildsCountForJob, self)._build_options(parser)
+        parser.add_argument('--name', required=True,
+                            help='The name of Jenkins job to check. Required parameter')
+        parser.add_argument('--depth',
+                            help='How many recent builds should we check for the particular job')
+        parser.set_defaults(depth=50)
+
+    def __call__(self):
+        parser = self._get_parser()
+        args = parser.parse_args()
+        job = self._jenkins.get_job(args.name)
+        running_builds_count = 0
+        try:
+            all_build_ids = job.get_build_ids()
+            counter = 0
+            for build_id in all_build_ids:
+                if counter >= int(args.depth):
+                    return running_builds_count
+                else:
+                    counter += 1
+                build = job.get_build_metadata(build_id)
+                if build.is_running():
+                    running_builds_count += 1
+            return running_builds_count
+        except Exception:
+            traceback.print_exc()
+            return running_builds_count
