d4275368dd9a81ad97b6e505348fd6b1610b79b5
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-01-29 15:37:09 +0100

    Added ability to skip job queue state check for some controller options

--- tests/tools/jenkins_controller/cli_handlers/is_build_running_for_job.py
+++ tests/tools/jenkins_controller/cli_handlers/is_build_running_for_job.py
@@ -11,7 +11,8 @@ class IsBuildRunningForJob(CliHandlerBase):
         parser.add_argument('--name', required=True,
                             help='The name of Jenkins job to check. Required parameter')
         parser.add_argument('--queue_timeout',
-                            help='Maximum time to wait while this job has queued items (in seconds, 300 by default)')
+                            help='Maximum time to wait while this job has queued items (in seconds, 300 by default). '\
+                            'Set it to negative value if you want to ignore this verification.')
         parser.add_argument('--build', required=True,
                             help='Build number to check. Required parameter')
         parser.set_defaults(queue_timeout=300)
