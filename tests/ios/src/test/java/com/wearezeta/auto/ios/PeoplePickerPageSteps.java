b9ab2ddf5e0eb4c141bd7d4d05b6be3281f2b6dd
Juliane Reschke <juliane.reschke@wearezeta.com>
2015-02-09 16:24:50 +0100

    step documentation

--- tests/ios/src/test/java/com/wearezeta/auto/ios/PeoplePickerPageSteps.java
+++ tests/ios/src/test/java/com/wearezeta/auto/ios/PeoplePickerPageSteps.java
@@ -279,6 +279,12 @@ public class PeoplePickerPageSteps {
 		PagesCollection.peoplePickerPage.tapOnPeoplePickerClearBtn();
 	}
 
+	/**
+	 * Unblocks a blocked user by clicking the unblock button
+	 * 
+	 * @step. I unblock user
+	 * 
+	 */
 	@When("^I unblock user$")
 	public void IUnblockUser() throws Exception {
 		PagesCollection.dialogPage = PagesCollection.peoplePickerPage.unblockUser();
