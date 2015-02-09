0ce3d64166aaa8a2035704b7679e3974f8bc86e1
Maksym Kuvshynov <maksym.kuvshynov@wearezeta.com>
2015-02-09 09:58:26 +0200

    Remove user test fix

--- tests/android/src/main/java/com/wearezeta/auto/android/pages/OtherUserPersonalInfoPage.java
+++ tests/android/src/main/java/com/wearezeta/auto/android/pages/OtherUserPersonalInfoPage.java
@@ -118,7 +118,7 @@ public class OtherUserPersonalInfoPage extends AndroidPage {
 	
 	public void clickBlockBtn() {
 		refreshUITree();
-		blockButton.click();
+		rightConversationButton.click();
 	}
 	
 	public AndroidPage clickUnblockBtn() throws Exception {
