c9da91b6c4effbfcb1b42e5c78755beb4200099d
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-02-25 10:02:54 +0100

    Trying to fix python exception from http://192.168.10.44:8080/job/webapp_master_test/206//console

--- tests/tools/jenkins_controller/cli_handlers/nodes_count_for_labels.py
+++ tests/tools/jenkins_controller/cli_handlers/nodes_count_for_labels.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import requests
 import xml.etree.ElementTree as ET
 
 from cli_handlers.cli_handler_base import CliHandlerBase
@@ -20,16 +21,21 @@ class NodesCountForLabels(CliHandlerBase):
         args = parser.parse_args()
         expected_labels = self._normalize_labels(args.labels.split(','))
         result_nodes_count = 0
-        for _, node in self._jenkins.get_nodes().iteritems():
-            if node.is_online():
-                response = node.jenkins.requester.get_and_confirm_status(
-                                                "%(baseurl)s/config.xml" % node.__dict__)
-                et = ET.fromstring(response.text)
-                node_labels_str = et.find('label').text
-                if node_labels_str:
-                    node_labels = self._normalize_labels(node_labels_str.split(' '))
-                else:
-                    node_labels = set()
-                if expected_labels.issubset(node_labels):
-                    result_nodes_count += 1
+        try:
+            for _, node in self._jenkins.get_nodes().iteritems():
+                if node.is_online():
+                    response = node.jenkins.requester.get_and_confirm_status(
+                                                    "%(baseurl)s/config.xml" % node.__dict__)
+                    et = ET.fromstring(response.text)
+                    node_labels_str = et.find('label').text
+                    if node_labels_str:
+                        node_labels = self._normalize_labels(node_labels_str.split(' '))
+                    else:
+                        node_labels = set()
+                    if expected_labels.issubset(node_labels):
+                        result_nodes_count += 1
+        except ImportError:
+            # nodes.py tries to import pdb in case of error
+            # intercepting this and raise connection error to retry on the higher level 
+            raise requests.exceptions.ConnectionError("Just retry")
         return result_nodes_count
