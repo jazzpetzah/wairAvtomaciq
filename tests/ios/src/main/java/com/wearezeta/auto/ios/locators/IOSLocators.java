0698808b800e56b18c2e1ea76c317c31c143c457
Juliane Reschke <juliane.reschke@wearezeta.com>
2015-02-26 10:49:15 +0100

    new locators for ping and hot ping in conv view

--- tests/ios/src/main/java/com/wearezeta/auto/ios/locators/IOSLocators.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/locators/IOSLocators.java
@@ -58,7 +58,8 @@ public final class IOSLocators {
 	
 	public static final String nameOpenConversationDetails = "conversationDetailButton";
 	
-	public static final String xpathYouPinged = "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[2]/UIAStaticText[contains(@name, 'YOU PINGED')]";
+	public static final String xpathYouPinged = "//UIAApplication[1]/UIAWindow[@name='ZClientMainWindow']/UIATableView[1]/UIATableCell[last()]/UIAStaticText[contains(@name, 'YOU PINGED')]";
+	public static final String xpathYouPingedAgain = "//UIAApplication[1]/UIAWindow[@name='ZClientMainWindow']/UIATableView[1]/UIATableCell[last()]/UIAStaticText[contains(@name, 'YOU PINGED AGAIN')]";
 	
 	public static final String xpathLastDialogMessage = "//UIAApplication[1]/UIAWindow[@name='ZClientMainWindow']/UIATableView[1]/UIATableCell[last()]";
 	
