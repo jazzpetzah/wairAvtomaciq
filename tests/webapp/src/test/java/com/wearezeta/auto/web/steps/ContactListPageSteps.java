3694845129ae376a2d7b6eaf585cdf995be968a4
Kirill Aleksandrov <Kyrylo.Aleksandrov@wearezeta.com>
2015-02-26 22:41:16 +0200

    Possible fix for not visible Mute button error on Safari

--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/ContactListPageSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/ContactListPageSteps.java
@@ -203,7 +203,7 @@ public class ContactListPageSteps {
 	public void IClickMuteButton(String contact) throws Exception {
 		contact = usrMgr.replaceAliasesOccurences(contact, FindBy.NAME_ALIAS);
 		PagesCollection.contactListPage.clickActionsButtonForContact(contact);
-		Thread.sleep(1000);
+		Thread.sleep(3000);
 		PagesCollection.contactListPage
 				.clickMuteConversationForContact(contact);
 	}
