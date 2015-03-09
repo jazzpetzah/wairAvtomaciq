a0a975358701688c06e824a056c2d668b4b4342b
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-03-09 12:34:45 +0100

    More advances sign in retry algorithm

--- tests/webapp/src/main/java/com/wearezeta/auto/web/pages/AuthorizationPage.java
+++ tests/webapp/src/main/java/com/wearezeta/auto/web/pages/AuthorizationPage.java
@@ -1,15 +1,21 @@
 package com.wearezeta.auto.web.pages;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.thoughtworks.selenium.SeleniumException;
 import com.wearezeta.auto.common.driver.DriverUtils;
 import com.wearezeta.auto.common.driver.ZetaWebAppDriver;
+import com.wearezeta.auto.common.log.ZetaLogger;
 import com.wearezeta.auto.web.locators.WebAppLocators;
 
 public class AuthorizationPage extends WebPage {
+
+	private static final Logger log = ZetaLogger.getLog(AuthorizationPage.class
+			.getSimpleName());
 
 	@FindBy(how = How.XPATH, using = WebAppLocators.AuthorizationPage.xpathCreateAccountButton)
 	private WebElement createAccount;
@@ -30,9 +36,27 @@ public class AuthorizationPage extends WebPage {
 	}
 
 	public LoginPage clickSignInButton() throws Exception {
-		DriverUtils.waitUntilElementVisible(driver, signInButton);
-		DriverUtils.waitUntilElementClickable(driver, signInButton);
-		signInButton.click();
+		final int maxTries = 3;
+		int ntry = 0;
+		do {
+			DriverUtils.waitUntilElementVisible(driver, signInButton);
+			DriverUtils.waitUntilElementClickable(driver, signInButton);
+			try {
+				signInButton.click();
+				break;
+			} catch (SeleniumException e) {
+				ntry++;
+				if (ntry < maxTries) {
+					log.debug(String
+							.format("Failed to click Sign In button. Trying to retry (%d of %d)...",
+									ntry + 1, maxTries));
+					e.printStackTrace();
+					Thread.sleep(1000);
+				} else {
+					throw e;
+				}
+			}
+		} while (ntry < maxTries);
 
 		return new LoginPage(this.getDriver(), this.getWait());
 	}
