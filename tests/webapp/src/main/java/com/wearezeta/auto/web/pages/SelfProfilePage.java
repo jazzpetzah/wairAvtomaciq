1df17644055c07c047e6eaaf704c82cdbe8acdbd
Kirill Aleksandrov <Kyrylo.Aleksandrov@wearezeta.com>
2015-03-11 16:22:31 +0200

    Fixed click on Open Search button in IE10

--- tests/webapp/src/main/java/com/wearezeta/auto/web/pages/SelfProfilePage.java
+++ tests/webapp/src/main/java/com/wearezeta/auto/web/pages/SelfProfilePage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.google.common.base.Function;
