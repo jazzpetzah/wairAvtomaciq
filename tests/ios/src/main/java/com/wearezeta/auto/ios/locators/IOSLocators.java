cdb8feb205a8871fb8cb7ca5eb1c9a58c6993ce4
Juliane Reschke <juliane.reschke@wearezeta.com>
2015-03-18 15:40:57 +0100

    added more specification to the xpath, to make it more reliable

--- tests/ios/src/main/java/com/wearezeta/auto/ios/locators/IOSLocators.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/locators/IOSLocators.java
@@ -246,7 +246,7 @@ public final class IOSLocators {
 	
 	public static final String nameOptionsHelpButton = "Help";
 	
-	public static final String xpathSettingsHelpHeader = "//UIAApplication[1]/UIAWindow[2]/UIAScrollView[1]/UIAScrollView[1]/UIAWebView[1]/UIALink[1]";
+	public static final String xpathSettingsHelpHeader = "//UIAApplication[@name='Safari']/UIAWindow[2]/UIAScrollView[1]/UIAScrollView[1]/UIAWebView[1]/UIALink[@name='Support']";
 	
 	///////////////////////
 	//Other User Profile
