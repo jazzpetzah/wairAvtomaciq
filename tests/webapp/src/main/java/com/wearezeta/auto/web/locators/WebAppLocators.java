d32d8a3646ee2996c7ecc98b62370bbfd22d8004
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-04-01 22:26:56 +0200

    Fixed css locator of incoming pending item in the convo list

--- tests/webapp/src/main/java/com/wearezeta/auto/web/locators/WebAppLocators.java
+++ tests/webapp/src/main/java/com/wearezeta/auto/web/locators/WebAppLocators.java
@@ -40,7 +40,7 @@ public final class WebAppLocators {
 		public static final String cssParentContactListItem = "div#conversation-list";
 
 		public static final String cssIncomingPendingConvoItem = cssParentContactListItem
-				+ " div[data-uie-name=item-pending-request]";
+				+ " [data-uie-name=item-pending-request]";
 
 		public static final String xpathOpenArchivedConvosButton = "//*[@data-uie-name='go-archive']";
 
