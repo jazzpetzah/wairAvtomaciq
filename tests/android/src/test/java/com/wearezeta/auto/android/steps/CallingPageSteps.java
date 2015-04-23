32f6543ace65d6881d9df9372d0f6645ff3acbad
deanrobertcook <deanrobertcook@gmail.com>
2015-04-23 18:22:30 +0200

    Added missing calling steps from package name change

--- tests/android/src/test/java/com/wearezeta/auto/android/steps/CallingPageSteps.java
+++ tests/android/src/test/java/com/wearezeta/auto/android/steps/CallingPageSteps.java
@@ -46,6 +46,22 @@ public class CallingPageSteps {
 	}
 
 	/**
+	 * Verify that started call message is visible
+	 * 
+	 * @step. ^I see started call message for contact (.*)$
+	 * @param contact
+	 *            contact name with whom you have a call
+	 * @throws Exception
+	 */
+	@When("^I see started call message for contact (.*)$")
+	public void ISeeStartedCallMesage(String contact) throws Exception {
+		contact = usrMgr.findUserByNameOrNameAlias(contact).getName();
+		String callersName = PagesCollection.callingOverlayPage
+				.getCallersName();
+		Assert.assertEquals(contact, callersName);
+	}
+
+	/**
 	 * Checks to see that the call bar is not visible
 	 * 
 	 * @step. ^I cannot see the call bar$
@@ -55,5 +71,54 @@ public class CallingPageSteps {
 	@When("^I cannot see the call bar$")
 	public void ICannotSeeTheCallBar() throws Exception {
 		Assert.assertFalse(PagesCollection.callingOverlayPage.isVisible());
+	}
+
+	/**
+	 * Checks to see that the calling lock screen appears when a user calls
+	 * while Wire is minimised or the phone is locked
+	 * 
+	 * @step. ^I see the call lock screen$
+	 * 
+	 * @throws Exception
+	 */
+	@When("I see the call lock screen$")
+	public void ISeeTheCallLockScreen() throws Exception {
+		PagesCollection.callingLockscreenPage = new CallingLockscreenPage(
+				PagesCollection.loginPage.getDriver(),
+				PagesCollection.loginPage.getWait());
+		Assert.assertFalse(PagesCollection.callingOverlayPage.isVisible());
+	}
+
+	/**
+	 * Checks to see that the user calling in the lock screen is the correct
+	 * user
+	 * 
+	 * @step. ^I see a call from (.*) in the call lock screen$
+	 * 
+	 * @param contact
+	 *            The username to compare the "is calling" message to.
+	 * 
+	 * @throws Exception
+	 */
+	@When("I see a call from (.*) in the call lock screen$")
+	public void ISeeACallFromUserInLockScreen(String contact) throws Exception {
+		contact = usrMgr.findUserByNameOrNameAlias(contact).getName();
+		String callersName = PagesCollection.callingLockscreenPage
+				.getCallersName();
+		Assert.assertEquals(contact, callersName);
+	}
+
+	/**
+	 * Answers the call from the lock screen and sets up the calling overlay
+	 * page
+	 * 
+	 * @step. ^I answer the call from the lock screen$
+	 * 
+	 * @throws Exception
+	 */
+	@When("I answer the call from the lock screen$")
+	public void IAnswerCallFromTheLockScreen() throws Exception {
+		PagesCollection.callingOverlayPage = PagesCollection.callingLockscreenPage
+				.acceptCall();
 	}
 }
