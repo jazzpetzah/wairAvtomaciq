94cdbe276e0027403bb7404845c9f5d088b98c4c
Michael Koppen <michael.koppen@googlemail.com>
2015-05-12 13:10:09 +0200

    Implemented QA-1059 / ID 1866 "Verify I can call a user for more than 15 mins"

--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.logging.LogEntries;
 import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
@@ -47,6 +46,10 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.commons.collections.IteratorUtils;
+import static org.junit.Assert.assertTrue;
 
 public class CommonWebAppSteps {
 	private final CommonSteps commonSteps = CommonSteps.getInstance();
@@ -664,12 +667,46 @@ public class CommonWebAppSteps {
 		}
 	}
 
-	private void writeBrowserLogsIntoMainLog(RemoteWebDriver driver) {
-		log.debug("BROWSER CONSOLE LOGS:");
-		LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
-		for (LogEntry logEntry : logEntries) {
-			log.debug(logEntry.getMessage());
+	/**
+	 * Verifies whether current browser log is empty or not
+	 *
+	 * @step. ^I verify browser log is empty$
+	 *
+	 * @throws Exception
+	 */
+	@Then("^I verify browser log is empty$")
+	public void VerifyBrowserLogIsEmpty() throws Exception {
+		if (PlatformDrivers.getInstance().hasDriver(CURRENT_PLATFORM)) {
+			try {
+				if (WebAppExecutionContext.LoggingManagement
+						.isSupportedInCurrentBrowser()) {
+					List<LogEntry> browserLog = getBrowserLog(PlatformDrivers
+							.getInstance()
+							.getDriver(CURRENT_PLATFORM)
+							.get(ZetaDriver.INIT_TIMEOUT_MILLISECONDS,
+									TimeUnit.MILLISECONDS));
+					assertTrue(browserLog.isEmpty());
+				}
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
 		}
+	}
+
+	private List<LogEntry> getBrowserLog(RemoteWebDriver driver) {
+		return IteratorUtils.toList((Iterator<LogEntry>) driver.manage().logs()
+				.get(LogType.BROWSER).iterator());
+	}
+
+	private void writeBrowserLogsIntoMainLog(RemoteWebDriver driver) {
+		List<LogEntry> logEntries = getBrowserLog(driver);
+		if (!logEntries.isEmpty()) {
+			log.debug("BROWSER CONSOLE LOGS:");
+			for (LogEntry logEntry : logEntries) {
+				log.debug(logEntry.getMessage());
+			}
+		}
+
 	}
 
 	@After
--- tests/webapp/src/test/resources/com/wearezeta/auto/web/Calling.feature
+++ tests/webapp/src/test/resources/com/wearezeta/auto/web/Calling.feature
@@ -50,7 +50,29 @@ Feature: Calling
 
     Examples: 
       | Login      | Password      | Name      | Contact   | CallBackend | Timeout |
-      | user1Email | user1Password | user1Name | user2Name | webdriver   | 120     |
+	 | user1Email | user1Password | user1Name | user2Name | webdriver   | 120     |
+
+  @staging @id1866
+   Scenario Outline: Verify I can call a user for more than 15 mins
+    Given My browser supports calling
+    Given There are 2 users where <Name> is me
+    Given Myself is connected to <Contact>
+    Given <Contact> starts waiting instance using <CallBackend>
+    Given <Contact> accepts next incoming call automatically
+    Given I Sign in using login <Login> and password <Password>
+    And I see my name on top of Contact list
+    And I open conversation with <Contact>
+    When I call
+    Then <Contact> verifies that waiting instance status is changed to active in <Timeout> seconds
+    And I wait for 900 seconds
+    And I see the calling bar
+    And I end the call
+    And I verify browser log is empty
+    And <Contact> stops all waiting instances
+
+    Examples: 
+ | Login      | Password      | Name      | Contact   | CallBackend | Timeout |
+ | user1Email | user1Password | user1Name | user2Name | webdriver   | 120     |
 
   # This has to work even in browsers, which don't support calling
   @regression @id2014
