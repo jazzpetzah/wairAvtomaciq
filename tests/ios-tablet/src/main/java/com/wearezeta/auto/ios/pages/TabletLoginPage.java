665c8c650e73236a540ec1f64c9aad9408e016bd
Juliane Reschke <juliane.reschke@wearezeta.com>
2015-05-15 12:12:42 +0200

    login page set up

--- tests/ios-tablet/src/main/java/com/wearezeta/auto/ios/pages/TabletLoginPage.java
+++ tests/ios-tablet/src/main/java/com/wearezeta/auto/ios/pages/TabletLoginPage.java
@@ -1,5 +1,14 @@
 package com.wearezeta.auto.ios.pages;
 
-public class TabletLoginPage {
+import java.util.concurrent.Future;
+
+import com.wearezeta.auto.common.driver.ZetaIOSDriver;
+
+public class TabletLoginPage extends LoginPage {
+	
+	public TabletLoginPage(Future<ZetaIOSDriver> lazyDriver)
+			throws Exception {
+		super(lazyDriver);
+	}
 
 }
