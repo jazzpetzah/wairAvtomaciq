fe362553cd928c30a5c7d82ac782536e4b662f97
Juliane Reschke <juliane.reschke@wearezeta.com>
2015-05-26 17:42:30 +0200

    new TabletConversationDetailPopoverPage and PageSteps

---
+++ tests/ios-tablet/src/main/java/com/wearezeta/auto/ios/pages/TabletConversationDetailPopoverPage.java
@@ -0,0 +1,14 @@
+package com.wearezeta.auto.ios.pages;
+
+import java.util.concurrent.Future;
+
+import com.wearezeta.auto.common.driver.ZetaIOSDriver;
+
+public class TabletConversationDetailPopoverPage extends OtherUserPersonalInfoPage {
+	
+	public TabletConversationDetailPopoverPage(Future<ZetaIOSDriver> lazyDriver) throws Exception {
+		super(lazyDriver);
+		// TODO Auto-generated constructor stub
+	}
+
+}
