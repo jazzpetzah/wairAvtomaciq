8aa80d18c661661255b03c4623170d5099d15d82
Tsareva-Lenskaya Darya <dasha@wearezeta.com>
2015-06-01 10:16:23 +0200

    Start working on verification for playing/pausing youtube link (id 2405)

--- tests/ios-tablet/src/test/java/com/wearezeta/auto/ios/steps/TabletDialogPageSteps.java
+++ tests/ios-tablet/src/test/java/com/wearezeta/auto/ios/steps/TabletDialogPageSteps.java
@@ -9,6 +9,7 @@ import cucumber.api.java.en.When;
 
 public class TabletDialogPageSteps {
 	
+	public String longMessage = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.";
 	/**
 	 * Presses the add picture button on iPad to open a CameraRollPopoverPage
 	 * @step. ^I press Add Picture button on iPad$
@@ -20,6 +21,13 @@ public class TabletDialogPageSteps {
 				.instantiatePage(TabletDialogPage.class);
 		CameraRollTabletPopoverPage page = TabletPagesCollection.tabletDialogPage.pressAddPictureiPadButton();
 		TabletPagesCollection.cameraRolliPadPopoverPage = (CameraRollTabletPopoverPage) page;
+	}
+	
+	@When("I type and send long message for tablet and media link (.*)")
+	public void ITypeAndSendLongTextAndMediaLink(String link) throws Exception {
+		PagesCollection.dialogPage.sendMessageUsingScript(longMessage);
+		Thread.sleep(1000);
+		PagesCollection.dialogPage.sendMessageUsingScript(link);
 	}
 
 }
