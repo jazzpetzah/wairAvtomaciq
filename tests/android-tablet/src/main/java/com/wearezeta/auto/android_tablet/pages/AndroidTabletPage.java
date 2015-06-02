62bae635bde853d316a9e93a316f66615dcd8ea1
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-06-02 14:57:00 +0200

    Some improvements for AndroidTabletPage

--- tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/AndroidTabletPage.java
+++ tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/AndroidTabletPage.java
@@ -51,4 +51,70 @@ public abstract class AndroidTabletPage extends AndroidPage {
 			throws Exception {
 		return null;
 	};
+
+	@Override
+	public AndroidTabletPage swipeRightCoordinates(int durationMilliseconds)
+			throws Exception {
+		return (AndroidTabletPage) super
+				.swipeRightCoordinates(durationMilliseconds);
+	}
+
+	@Override
+	public AndroidTabletPage swipeRightCoordinates(int durationMilliseconds,
+			int horizontalPercent) throws Exception {
+		return (AndroidTabletPage) super.swipeRightCoordinates(
+				durationMilliseconds, horizontalPercent);
+	}
+
+	@Override
+	public AndroidTabletPage swipeLeftCoordinates(int durationMilliseconds)
+			throws Exception {
+		return (AndroidTabletPage) super
+				.swipeLeftCoordinates(durationMilliseconds);
+	}
+
+	@Override
+	public AndroidTabletPage swipeLeftCoordinates(int durationMilliseconds,
+			int horizontalPercent) throws Exception {
+		return (AndroidTabletPage) super.swipeLeftCoordinates(
+				durationMilliseconds, horizontalPercent);
+	}
+
+	@Override
+	public AndroidTabletPage swipeUpCoordinates(int durationMilliseconds)
+			throws Exception {
+		return (AndroidTabletPage) super
+				.swipeUpCoordinates(durationMilliseconds);
+	}
+
+	@Override
+	public AndroidTabletPage swipeUpCoordinates(int durationMilliseconds,
+			int verticalPercent) throws Exception {
+		return (AndroidTabletPage) super.swipeUpCoordinates(
+				durationMilliseconds, verticalPercent);
+	}
+
+	@Override
+	public AndroidTabletPage swipeByCoordinates(int durationMilliseconds,
+			int widthStartPercent, int hightStartPercent, int widthEndPercent,
+			int hightEndPercent) throws Exception {
+		return (AndroidTabletPage) super.swipeByCoordinates(
+				durationMilliseconds, widthStartPercent, hightStartPercent,
+				widthEndPercent, hightEndPercent);
+	}
+
+	@Override
+	public AndroidTabletPage swipeDownCoordinates(int durationMilliseconds)
+			throws Exception {
+		return (AndroidTabletPage) super
+				.swipeDownCoordinates(durationMilliseconds);
+	}
+
+	@Override
+	public AndroidTabletPage swipeDownCoordinates(int durationMilliseconds,
+			int verticalPercent) throws Exception {
+		return (AndroidTabletPage) super.swipeDownCoordinates(
+				durationMilliseconds, verticalPercent);
+	}
+
 }
