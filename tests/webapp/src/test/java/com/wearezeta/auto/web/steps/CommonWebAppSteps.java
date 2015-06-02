3fc95be2fe8b1208a7d1c6e9fe8d30e560d96966
Sven Jost <sven@wire.com>
2015-06-02 20:25:48 +0200

    Set headless environment

--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
@@ -72,7 +72,7 @@ public class CommonWebAppSteps {
 	private static final String DEFAULT_USER_PICTURE = PerformanceCommon.DEFAULT_PERF_IMAGE;
 
 	static {
-		System.setProperty("java.awt.headless", "false");
+		System.setProperty("java.awt.headless", "true");
 		System.setProperty("org.apache.commons.logging.Log",
 				"org.apache.commons.logging.impl.SimpleLog");
 		System.setProperty(
