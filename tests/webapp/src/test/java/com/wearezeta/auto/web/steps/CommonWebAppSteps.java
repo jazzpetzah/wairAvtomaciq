f79a50417603d4700a488389de7394d20fa044b7
Sven Jost <sven@wire.com>
2015-06-03 16:27:42 +0200

    Better unique test name

--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
@@ -320,8 +320,8 @@ public class CommonWebAppSteps {
 		String browserVersion = System.getProperty("com.wire.browser.version");
 		String platform = getPlatform();
 
-		return scenario.getName() + scenario.getId() + " on " + platform
-				+ " with " + browserName + " " + browserVersion;
+		return scenario.getName() + " (" + scenario.getId() + ") on "
+				+ platform + " with " + browserName + " " + browserVersion;
 	}
 
 	private String getPlatform() {
