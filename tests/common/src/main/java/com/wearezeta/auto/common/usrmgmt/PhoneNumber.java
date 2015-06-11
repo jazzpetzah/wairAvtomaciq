57369812eff8b1230426edbfbc263a35eec9a9a7
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-06-11 10:35:56 +0200

    Fixed max length of the random phone number

--- tests/common/src/main/java/com/wearezeta/auto/common/usrmgmt/PhoneNumber.java
+++ tests/common/src/main/java/com/wearezeta/auto/common/usrmgmt/PhoneNumber.java
@@ -58,8 +58,10 @@ public final class PhoneNumber {
 		final Random rand = new Random();
 		// The very first digit should not be Zero
 		result.append(Integer.toString(1 + rand.nextInt(9)));
-		for (int i = 1; i <= MIN_NUMBER_LENGTH - prefix.length()
-				+ rand.nextInt(MAX_NUMBER_LENGTH - MIN_NUMBER_LENGTH - 1); i++) {
+		for (int i = 0; i < MIN_NUMBER_LENGTH
+				- prefix.length()
+				+ rand.nextInt(MAX_NUMBER_LENGTH - MIN_NUMBER_LENGTH
+						+ prefix.length() - 1); i++) {
 			result.append(Integer.toString(rand.nextInt(10)));
 		}
 		return result.toString();
