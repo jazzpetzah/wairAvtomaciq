ee9e13a0fa5ab5fe75316b66e3808b7fd1d7b38a
Sven Jost <sven@wire.com>
2015-06-19 14:31:55 +0200

    Implementation of testcase.id=id2716

--- tests/webapp/src/main/java/com/wearezeta/auto/web/pages/PhoneNumberVerificationPage.java
+++ tests/webapp/src/main/java/com/wearezeta/auto/web/pages/PhoneNumberVerificationPage.java
@@ -2,15 +2,26 @@ package com.wearezeta.auto.web.pages;
 
 import java.util.concurrent.Future;
 
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import com.wearezeta.auto.common.driver.DriverUtils;
 import com.wearezeta.auto.common.driver.ZetaWebAppDriver;
+import com.wearezeta.auto.common.log.ZetaLogger;
+import com.wearezeta.auto.web.locators.WebAppLocators;
 
 public class PhoneNumberVerificationPage extends WebPage {
 
+	private static final Logger log = ZetaLogger
+			.getLog(PhoneNumberVerificationPage.class.getSimpleName());
+
 	@FindBy(css = "[data-uie-name='enter-phone-code']")
 	private WebElement phoneCodeField;
+
+	@FindBy(css = WebAppLocators.PhoneNumberVerificationPage.cssErrorMessage)
+	private WebElement errorMessage;
 
 	public PhoneNumberVerificationPage(Future<ZetaWebAppDriver> lazyDriver)
 			throws Exception {
@@ -23,8 +34,17 @@ public class PhoneNumberVerificationPage extends WebPage {
 	}
 
 	public ContactListPage enterCode(String code) throws Exception {
+		log.info("Enter code: " + code);
 		phoneCodeField.clear();
 		phoneCodeField.sendKeys(code);
 		return new ContactListPage(getLazyDriver());
+	}
+
+	public String getErrorMessage() throws Exception {
+		DriverUtils
+				.waitUntilLocatorAppears(
+						getDriver(),
+						By.xpath(WebAppLocators.PhoneNumberVerificationPage.cssErrorMessage));
+		return errorMessage.getText();
 	}
 }
--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/PhoneNumberVerificationPageSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/PhoneNumberVerificationPageSteps.java
@@ -1,5 +1,8 @@
 package com.wearezeta.auto.web.steps;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.apache.log4j.Logger;
 
 import com.wearezeta.auto.common.backend.BackendAPIWrappers;
@@ -9,6 +12,7 @@ import com.wearezeta.auto.common.usrmgmt.ClientUsersManager;
 import com.wearezeta.auto.web.pages.PagesCollection;
 import com.wearezeta.auto.web.pages.PhoneNumberVerificationPage;
 
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class PhoneNumberVerificationPageSteps {
@@ -29,4 +33,25 @@ public class PhoneNumberVerificationPageSteps {
 				.enterCode(code);
 	}
 
+	@When("^I enter wrong phone verification code for user (.*)$")
+	public void i_enter_wrong_phone_verification_code_for_user_user_Name(
+			String name) throws Throwable {
+		ClientUser user = usrMgr.findUserByNameOrNameAlias(name);
+		String code = BackendAPIWrappers.getLoginCodeByPhoneNumber(user
+				.getPhoneNumber());
+		String wrongcode = code.replace("0", "1");
+		wrongcode = wrongcode.replace("2", "3");
+		wrongcode = wrongcode.replace("4", "5");
+		wrongcode = wrongcode.replace("6", "7");
+		wrongcode = wrongcode.replace("8", "9");
+		PagesCollection.contactListPage = PagesCollection.phoneNumberVerificationPage
+				.enterCode(wrongcode);
+	}
+
+	@Then("^I see invalid phone code error message saying (.*)")
+	public void TheSignInErrorMessageReads(String message) throws Exception {
+		assertThat("invalid phone code error",
+				PagesCollection.phoneNumberVerificationPage.getErrorMessage(),
+				equalTo(message));
+	}
 }
