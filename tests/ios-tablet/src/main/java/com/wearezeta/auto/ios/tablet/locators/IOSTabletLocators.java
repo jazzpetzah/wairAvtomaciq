5918ba0a368f97e81f1f67cd23095128e9064161
Juliane Reschke <juliane.reschke@wearezeta.com>
2015-06-30 17:33:21 +0200

    locators for the amount of people in the conversation

--- tests/ios-tablet/src/main/java/com/wearezeta/auto/ios/tablet/locators/IOSTabletLocators.java
+++ tests/ios-tablet/src/main/java/com/wearezeta/auto/ios/tablet/locators/IOSTabletLocators.java
@@ -33,6 +33,8 @@ public final class IOSTabletLocators {
 	
 	public static final class TabletGroupConversationDetailPopoverPage{
 		public static final String nameRenameButtonEllipsisMenue = "RENAME";
+		public static final String xpathGroupConvTotalNumber = "//UIAApplication[1]/UIAWindow[@name='ZClientMainWindow']/UIAPopover[1]/UIAStaticText[contains(@name,'PEOPLE')]";
+		public static final String namePeopleCountWord = " PEOPLE";
 	}
 	
 }
