cdbe4a2ea5608bda21904dbb9d635ba6e0819c0f
Juliane Reschke <juliane.reschke@wearezeta.com>
2015-07-07 18:07:42 +0200

    methods for this stuff before

--- tests/ios-tablet/src/main/java/com/wearezeta/auto/ios/pages/TabletOtherUserInfoPage.java
+++ tests/ios-tablet/src/main/java/com/wearezeta/auto/ios/pages/TabletOtherUserInfoPage.java
@@ -2,17 +2,27 @@ package com.wearezeta.auto.ios.pages;
 
 import java.util.concurrent.Future;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import com.wearezeta.auto.common.driver.DriverUtils;
 import com.wearezeta.auto.common.driver.ZetaIOSDriver;
+import com.wearezeta.auto.ios.locators.IOSLocators;
 import com.wearezeta.auto.ios.tablet.locators.IOSTabletLocators;
 
 public class TabletOtherUserInfoPage extends OtherUserPersonalInfoPage{
 	
 	@FindBy(how = How.NAME, using = IOSTabletLocators.TabletOtherUserInfoPage.nameOtherUserMetaControllerRightButtoniPadPopover)
 	private WebElement removeFromGroupChat;
+	
+	@FindBy(how = How.XPATH, using = IOSTabletLocators.TabletOtherUserInfoPage.xpathOtherUserNameField)
+	private WebElement nameFieldPopover;
+	
+	@FindBy(how = How.XPATH, using = IOSTabletLocators.TabletOtherUserInfoPage.xpathOtherUserEmailField)
+	private WebElement emailFieldPopover;
 
 	public TabletOtherUserInfoPage(Future<ZetaIOSDriver> lazyDriver)
 			throws Exception {
@@ -22,6 +32,21 @@ public class TabletOtherUserInfoPage extends OtherUserPersonalInfoPage{
 	
 	public void removeFromConversationOniPad() throws Exception{
 		removeFromGroupChat.click();
+	}
+	
+	public String getNameFieldValueOniPadPopover() {
+		return nameFieldPopover.getAttribute("value");
+	}
+
+	public String getEmailFieldValueOniPadPopover() throws Exception {
+		String result = "";
+		try {
+			DriverUtils.waitUntilLocatorAppears(getDriver(), By.xpath(IOSLocators.xpathOtherPersonalInfoPageEmailField));
+			result = emailFieldPopover.getAttribute("value");
+		} catch (NoSuchElementException ex) {
+
+		}
+		return result;
 	}
 
 }
