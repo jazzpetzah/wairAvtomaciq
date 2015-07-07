bfd097831b41e20069e547132d55775865fa74a0
Sven Jost <sven@wire.com>
2015-07-07 15:54:52 +0200

    [Webapp] Write xml junit reports with cucumber junitformatter

--- tests/webapp/src/test/java/com/wearezeta/auto/web/StagingRun.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/StagingRun.java
@@ -8,6 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(format = { "html:target/report",
 		"json:target/staging_report.json",
+		"junit:target/surefire-reports/TEST-com.wearezeta.auto.web.StagingRun.xml",
 		"com.wearezeta.auto.common.ZetaFormatter" }, tags = { "@staging" })
 public class StagingRun {
 
