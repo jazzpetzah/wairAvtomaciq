b65c873a7355386986a1eaf8aabb649371622200
Kirill Aleksandrov <Kyrylo.Aleksandrov@wearezeta.com>
2015-07-09 16:20:47 +0300

    Fixed android performance test to listen for android logs correctly and added debug output

--- tests/android/src/test/java/com/wearezeta/auto/android/steps/PerformanceSteps.java
+++ tests/android/src/test/java/com/wearezeta/auto/android/steps/PerformanceSteps.java
@@ -177,6 +177,7 @@ public class PerformanceSteps {
 				break;
 		}
 		getDialogPage().isDialogVisible();
+		Thread.sleep(10000);
 		CommonAndroidSteps.listener.stopListeningLogcat();
 		for (int y = 0; y < 2; y++) {
 			getDialogPage().swipeDown(DEFAULT_SWIPE_TIME);
@@ -199,6 +200,7 @@ public class PerformanceSteps {
 				AndroidPerformanceReportGenerator.RXLOG_FILEPATH,
 				RXLOGGER_RESOURCE_FILE_PATH);
 		Thread.sleep(5000);
+		log.debug(CommonAndroidSteps.listener.getOutput());
 		Assert.assertTrue(AndroidPerformanceReportGenerator
 				.updateReportDataWithCurrentRun(CommonAndroidSteps.listener
 						.getOutput()));
