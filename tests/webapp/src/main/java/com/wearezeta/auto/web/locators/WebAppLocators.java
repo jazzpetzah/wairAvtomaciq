a1812a8b8a8abd02182761d2654973e0d7de687d
Michael Koppen <michael.koppen@googlemail.com>
2015-07-28 19:05:05 +0200

    Step "I see the calling bar from user <User>" can now detect users during a call and currently calling users

--- tests/webapp/src/main/java/com/wearezeta/auto/web/locators/WebAppLocators.java
+++ tests/webapp/src/main/java/com/wearezeta/auto/web/locators/WebAppLocators.java
@@ -241,8 +241,10 @@ public final class WebAppLocators {
 
 		public static final Function<String, String> xpathCallingBarRootByName = text -> String
 				.format(xpathCallingBarRoot
-						+ "/div/div/div/div/div[contains(@class, 'cc-avatar-label') and text()='%s']",
-						text);
+						+ "/div/div/div/div/div[contains(@class, 'cc-avatar-label') and text()='%s']|"
+						+ xpathCallingBarRoot
+						+ "/div/div/div/span[contains(@class, 'cc-label-bold') and text()='%s']",
+						text, text);
 
 		public static String xpathAcceptCallButton = xpathCallingBarRoot
 				+ "//*[contains(@class, 'icon-check')]";
