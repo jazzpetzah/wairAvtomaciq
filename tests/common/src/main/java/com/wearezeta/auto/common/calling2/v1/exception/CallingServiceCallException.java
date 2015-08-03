958c96c0fb1fbacd74e0e359aa39318c384c32ba
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-08-03 11:04:27 +0200

    Fixed compiler warnings

--- tests/common/src/main/java/com/wearezeta/auto/common/calling2/v1/exception/CallingServiceCallException.java
+++ tests/common/src/main/java/com/wearezeta/auto/common/calling2/v1/exception/CallingServiceCallException.java
@@ -4,6 +4,8 @@ import com.wearezeta.auto.common.rest.RESTError;
 
 public class CallingServiceCallException extends RESTError {
 
+	private static final long serialVersionUID = -4831305426342617547L;
+
 	public CallingServiceCallException(String message, int returnCode) {
 		super(message, returnCode);
 	}
