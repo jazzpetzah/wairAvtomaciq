20c9bd5352147fca9a1fc40301e29b5f4cc7c7ae
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-08-03 12:26:02 +0200

    Normalise job name for usage with Jenkins API module

--- tests/tools/jenkins_controller/cli_handlers/is_any_node_with_labels_running_job.py
+++ tests/tools/jenkins_controller/cli_handlers/is_any_node_with_labels_running_job.py
@@ -28,7 +28,8 @@ class IsAnyNodeWithLabelsRunningJob(CliHandlerBase):
     def _invoke(self):
         parser = self._get_parser()
         args = parser.parse_args()
-        job = self._jenkins.get_job(args.name)
+        job_name = self._normalize_job_name(args.name)
+        job = self._jenkins.get_job(job_name)
         self._wait_while_job_in_queue(job, args.queue_timeout)
         try:
             all_build_ids = job.get_build_ids()
