227fb265f000cb36abd9ac1d397f18f84496d7b8
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-08-11 18:39:39 +0200

    Automated tablet test cases ## 2910, 3123

--- tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/TabletCallingOverlayPage.java
+++ tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/TabletCallingOverlayPage.java
@@ -1,8 +1,13 @@
 package com.wearezeta.auto.android_tablet.pages;
 
+import java.awt.image.BufferedImage;
+import java.util.Optional;
 import java.util.concurrent.Future;
 
+import org.openqa.selenium.By;
+
 import com.wearezeta.auto.android.pages.CallingOverlayPage;
+import com.wearezeta.auto.common.driver.DriverUtils;
 import com.wearezeta.auto.common.driver.ZetaAndroidDriver;
 
 public class TabletCallingOverlayPage extends AndroidTabletPage {
@@ -27,5 +32,35 @@ public class TabletCallingOverlayPage extends AndroidTabletPage {
 
 	public boolean incomingCallerAvatarIsInvisible() throws Exception {
 		return getCallingOverlayPage().incomingCallerAvatarIsInvisible();
+	}
+
+	public void tapAcceptButton() throws Exception {
+		getCallingOverlayPage().acceptCall();
+	}
+
+	public void tapMuteButton() throws Exception {
+		getCallingOverlayPage().tapMuteMicButton();
+	}
+
+	public void tapSpeakerButton() throws Exception {
+		getCallingOverlayPage().tapSpeakerButton();
+	}
+
+	public Optional<BufferedImage> getMuteButtonScreenshot() throws Exception {
+		return this.getElementScreenshot(getCallingOverlayPage().muteMicButton);
+	}
+
+	public boolean waitUntilSpeakerButtonVisible() throws Exception {
+		return DriverUtils.waitUntilLocatorIsDisplayed(getDriver(),
+				By.id(CallingOverlayPage.idCallingSpeaker));
+	}
+
+	public boolean waitUntilSpeakerButtonInvisible() throws Exception {
+		return DriverUtils.waitUntilLocatorDissapears(getDriver(),
+				By.id(CallingOverlayPage.idCallingSpeaker));
+	}
+
+	public void tapDismissButton() throws Exception {
+		getCallingOverlayPage().tapDismissButton();
 	}
 }
