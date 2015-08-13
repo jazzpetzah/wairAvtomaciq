6eb01743e5d8e76b6be05828694d9b0b07356a5e
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-08-13 15:43:14 +0200

    Addressed review comment

--- tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/popovers/NonConnectedUserIncomingConnectionPage.java
+++ tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/popovers/NonConnectedUserIncomingConnectionPage.java
@@ -2,17 +2,21 @@ package com.wearezeta.auto.android_tablet.pages.popovers;
 
 import java.util.concurrent.Future;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-import com.wearezeta.auto.common.driver.DriverUtils;
 import com.wearezeta.auto.common.driver.ZetaAndroidDriver;
 
 class NonConnectedUserIncomingConnectionPage extends
 		AbstractIncomingConnectionPage {
 
 	private static final String idAcceptButton = "zb__connect_request__accept_button";
-	@SuppressWarnings("unused")
+	@FindBy(id = idAcceptButton)
+	private WebElement acceptButton;
+
 	private static final String idIgnoreButton = "zb__connect_request__ignore_button";
+	@FindBy(id = idIgnoreButton)
+	private WebElement ignoreButton;
 
 	public NonConnectedUserIncomingConnectionPage(
 			Future<ZetaAndroidDriver> lazyDriver,
@@ -21,9 +25,7 @@ class NonConnectedUserIncomingConnectionPage extends
 	}
 
 	public void tapAcceptButton() throws Exception {
-		assert DriverUtils.waitUntilLocatorIsDisplayed(getDriver(),
-				By.id(idAcceptButton)) : "Connect button is not visible on the popover";
-		getDriver().findElement(By.id(idAcceptButton)).click();
+		acceptButton.click();
 	}
 
 }
