3e0221cef1a938f691a1af10fafd258f390f0306
Michael Koppen <michael.koppen@googlemail.com>
2015-08-19 18:22:33 +0200

    Fixed flows

--- tests/common/src/main/java/com/wearezeta/auto/common/calling2/v1/model/Flow.java
+++ tests/common/src/main/java/com/wearezeta/auto/common/calling2/v1/model/Flow.java
@@ -1,11 +1,19 @@
 package com.wearezeta.auto.common.calling2.v1.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Flow {
 	private final Long bytesIn;
 	private final Long bytesOut;
 	private final String remoteUserId;
 
-	public Flow(Long bytesIn, Long bytesOut, String remoteUserId) {
+	@JsonCreator
+	public Flow(@JsonProperty("bytesIn") Long bytesIn,
+			@JsonProperty("bytesOut") Long bytesOut,
+			@JsonProperty("remoteUserId") String remoteUserId) {
 		this.bytesIn = bytesIn;
 		this.bytesOut = bytesOut;
 		this.remoteUserId = remoteUserId;
@@ -22,4 +30,11 @@ public class Flow {
 	public String getRemoteUserId() {
 		return remoteUserId;
 	}
+
+	@Override
+	public String toString() {
+		return "Flow{" + "bytesIn=" + bytesIn + ", bytesOut=" + bytesOut
+				+ ", remoteUserId=" + remoteUserId + '}';
+	}
+
 }
