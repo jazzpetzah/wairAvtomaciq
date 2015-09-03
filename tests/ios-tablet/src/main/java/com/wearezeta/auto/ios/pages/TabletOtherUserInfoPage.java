19b43ffa55c05661915826b7f0e47f0b71e20aed
Piotr <piotr@wire.com>
2015-09-03 14:02:05 +0300

    iPad fixes

--- tests/ios-tablet/src/main/java/com/wearezeta/auto/ios/pages/TabletOtherUserInfoPage.java
+++ tests/ios-tablet/src/main/java/com/wearezeta/auto/ios/pages/TabletOtherUserInfoPage.java
@@ -13,27 +13,33 @@ import com.wearezeta.auto.common.driver.ZetaIOSDriver;
 import com.wearezeta.auto.ios.locators.IOSLocators;
 import com.wearezeta.auto.ios.tablet.locators.IOSTabletLocators;
 
-public class TabletOtherUserInfoPage extends OtherUserPersonalInfoPage{
-	
+public class TabletOtherUserInfoPage extends OtherUserPersonalInfoPage {
+
 	@FindBy(how = How.NAME, using = IOSTabletLocators.TabletOtherUserInfoPage.nameOtherUserMetaControllerRightButtoniPadPopover)
 	private WebElement removeFromGroupChat;
-	
+
 	@FindBy(how = How.XPATH, using = IOSTabletLocators.TabletOtherUserInfoPage.xpathOtherUserNameField)
 	private WebElement nameFieldPopover;
-	
+
 	@FindBy(how = How.XPATH, using = IOSTabletLocators.TabletOtherUserInfoPage.xpathOtherUserEmailField)
 	private WebElement emailFieldPopover;
+
+	@FindBy(how = How.XPATH, using = IOSTabletLocators.TabletOtherUserInfoPage.xpathOtherUserConnectLabel)
+	private WebElement connectLabel;
+	
+	@FindBy(how = How.XPATH, using = IOSTabletLocators.TabletOtherUserInfoPage.xpathOtherUserConnectButton)
+	private WebElement connectButton;
 
 	public TabletOtherUserInfoPage(Future<ZetaIOSDriver> lazyDriver)
 			throws Exception {
 		super(lazyDriver);
 		// TODO Auto-generated constructor stub
 	}
-	
-	public void removeFromConversationOniPad() throws Exception{
+
+	public void removeFromConversationOniPad() throws Exception {
 		removeFromGroupChat.click();
 	}
-	
+
 	public String getNameFieldValueOniPadPopover() {
 		return nameFieldPopover.getAttribute("value");
 	}
@@ -41,12 +47,41 @@ public class TabletOtherUserInfoPage extends OtherUserPersonalInfoPage{
 	public String getEmailFieldValueOniPadPopover() throws Exception {
 		String result = "";
 		try {
-			DriverUtils.waitUntilLocatorAppears(getDriver(), By.xpath(IOSLocators.xpathOtherPersonalInfoPageEmailField));
+			DriverUtils.waitUntilLocatorAppears(getDriver(),
+					By.xpath(IOSLocators.xpathOtherPersonalInfoPageEmailField));
 			result = emailFieldPopover.getAttribute("value");
 		} catch (NoSuchElementException ex) {
 
 		}
 		return result;
+	}
+
+	public boolean isConnectLabelVisible() throws Exception {
+		DriverUtils
+				.waitUntilLocatorAppears(
+						getDriver(),
+						By.xpath(IOSTabletLocators.TabletOtherUserInfoPage.xpathOtherUserConnectLabel),
+						5);
+		return DriverUtils.isElementPresentAndDisplayed(getDriver(),
+				connectLabel);
+	}
+
+	public void clickConnectLabel() {
+		connectLabel.click();
+	}
+	
+	public boolean isConnectButtonVisible() throws Exception {
+		DriverUtils
+				.waitUntilLocatorAppears(
+						getDriver(),
+						By.xpath(IOSTabletLocators.TabletOtherUserInfoPage.xpathOtherUserConnectButton),
+						5);
+		return DriverUtils.isElementPresentAndDisplayed(getDriver(),
+				connectButton);
+	}
+
+	public void clickConnectButton() {
+		connectButton.click();
 	}
 
 }
