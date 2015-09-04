2c9eac9cab194478b243245aa5b2283c9c331f62
Piotr <piotr@wire.com>
2015-09-04 13:09:30 +0300

    fix for about close button

--- tests/ios/src/main/java/com/wearezeta/auto/ios/locators/IOSLocators.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/locators/IOSLocators.java
@@ -281,7 +281,7 @@ public final class IOSLocators {
 
 	public static final String xpathAboutPageWireLogo = "//UIAApplication/UIAWindow/UIAButton[@name='wire.com']/preceding-sibling::UIAImage[1]";
 
-	public static final String xpathAboutCloseButton = "//UIAApplication/UIAWindow/UIAButton[1]";
+	public static final String xpathAboutCloseButton = "//UIAButton[@name='wire.com']/preceding-sibling::UIAButton[1]";
 
 	public static final String nameWireWebsiteButton = "wire.com";
 
