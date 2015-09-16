8a837576b7ad48a0f854591a417c0c382d61fb97
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2015-09-16 17:01:49 +0200

    Fixed Invitation link generator class

--- tests/common/src/main/java/com/wearezeta/auto/common/InvitationLinkGenerator.java
+++ tests/common/src/main/java/com/wearezeta/auto/common/InvitationLinkGenerator.java
@@ -1,16 +1,19 @@
 package com.wearezeta.auto.common;
 
+import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.security.*;
 import java.util.Random;
 import java.util.UUID;
+
 import javax.crypto.*;
 import javax.crypto.spec.*;
+
+import jersey.repackaged.com.google.common.base.Throwables;
+
 import org.apache.commons.codec.binary.Base64;
 
 public class InvitationLinkGenerator {
-	private final static String BASEURL = "https://staging-website.wire.com/c/";
 	private static Long TimeZero = 1388534400000L; // 2014.1.1 00:00:00 UTC
 	private static byte Secret[] = { 0x64, 0x68, (byte) 0xca, (byte) 0xee,
 			0x5c, 0x0, 0x25, (byte) 0xf5, 0x68, (byte) 0xe4, (byte) 0xd0,
@@ -21,25 +24,18 @@ public class InvitationLinkGenerator {
 	private static byte[] IV = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 			0, 0, 0, 0 };
 
-	private static Cipher getCipher(int mode) throws NoSuchAlgorithmException,
-			NoSuchPaddingException, InvalidKeyException,
-			InvalidAlgorithmParameterException {
+	private static Cipher getCipher(int mode) throws Exception {
 		Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
 		cipher.init(mode, new SecretKeySpec(Secret, "AES"),
 				new IvParameterSpec(IV));
 		return cipher;
 	}
 
-	private static Cipher encipher() throws InvalidKeyException,
-			NoSuchAlgorithmException, NoSuchPaddingException,
-			InvalidAlgorithmParameterException {
+	private static Cipher encipher() throws Exception {
 		return getCipher(Cipher.ENCRYPT_MODE);
 	}
 
-	private static String encodeToken(String userId)
-			throws IllegalBlockSizeException, BadPaddingException,
-			InvalidKeyException, NoSuchAlgorithmException,
-			NoSuchPaddingException, InvalidAlgorithmParameterException {
+	private static String encodeToken(String userId) throws Exception {
 		Long currentTime = System.currentTimeMillis();
 		byte[] bytes = new byte[32];
 		new Random().nextBytes(bytes);
@@ -60,17 +56,38 @@ public class InvitationLinkGenerator {
 		return (short) (Math.max(0, (currentTime - TimeZero) / 1000 / 3600));
 	}
 
-	public static String getInvitationUrl(String uuid)
-			throws InvalidKeyException, IllegalBlockSizeException,
-			BadPaddingException, NoSuchAlgorithmException,
-			NoSuchPaddingException, InvalidAlgorithmParameterException {
-		return BASEURL + encodeToken(uuid);
+	public static String getInvitationUrl(String uuid) throws Exception {
+		final String currentBackendType = CommonUtils
+				.getBackendType(InvitationLinkGenerator.class);
+		switch (currentBackendType.toLowerCase()) {
+		case "staging":
+			return String.format("https://staging-website.zinfra.io/c/%s",
+					encodeToken(uuid));
+		case "production":
+			return String
+					.format("https://app.wire.com/c/%s", encodeToken(uuid));
+		default:
+			throw new RuntimeException(String.format(
+					"Non supported backend type '%s'", currentBackendType));
+		}
 	}
 
-	public static String getInvitationToken(String uuid)
-			throws InvalidKeyException, IllegalBlockSizeException,
-			BadPaddingException, NoSuchAlgorithmException,
-			NoSuchPaddingException, InvalidAlgorithmParameterException {
+	public static String getInvitationToken(String uuid) throws Exception {
 		return encodeToken(uuid);
+	}
+
+	static {
+		// https://github.com/jruby/jruby/wiki/UnlimitedStrengthCrypto
+		try {
+			final Class<?> securityClass = Class
+					.forName("javax.crypto.JceSecurity");
+			final Field restrictedField = securityClass
+					.getDeclaredField("isRestricted");
+			restrictedField.setAccessible(true);
+			restrictedField.set(null, false);
+		} catch (Exception e) {
+			e.printStackTrace();
+			Throwables.propagate(e);
+		}
 	}
 }
