a16f7e6b4a4b30a40dbf833ded6adb1306af88a1
Juliane Reschke <juliane.reschke@wearezeta.com>
2015-09-29 15:54:15 +0200

    click Not Now Button

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/LoginPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/LoginPage.java
@@ -114,6 +114,9 @@ public class LoginPage extends IOSPage {
 
 	@FindBy(how = How.NAME, using = IOSLocators.Alerts.nameInvalidPhoneNumber)
 	private WebElement invalidPhoneNumberAlert;
+	
+	@FindBy(how = How.NAME, using = IOSLocators.PeoplePickerPage.nameNotNowButton)
+	private WebElement notNowPhoneButton;
 
 	private String login;
 
@@ -377,5 +380,9 @@ public class LoginPage extends IOSPage {
 		DriverUtils.waitUntilAlertAppears(getDriver());
 		return DriverUtils.isElementPresentAndDisplayed(getDriver(),
 				invalidPhoneNumberAlert);
+	}
+
+	public void clickPhoneNotNow() {
+		notNowPhoneButton.click();
 	}
 }
