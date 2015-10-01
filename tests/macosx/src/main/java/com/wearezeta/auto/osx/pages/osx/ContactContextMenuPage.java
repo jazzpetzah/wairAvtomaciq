70d5f61e1258c95375e554be4e6f29ac106e92dc
Michael Koppen <michael.koppen@googlemail.com>
2015-10-01 17:03:00 +0200

    Common improvements for wrapper test suite

--- tests/macosx/src/main/java/com/wearezeta/auto/osx/pages/osx/ContactContextMenuPage.java
+++ tests/macosx/src/main/java/com/wearezeta/auto/osx/pages/osx/ContactContextMenuPage.java
@@ -3,24 +3,18 @@ package com.wearezeta.auto.osx.pages.osx;
 import com.wearezeta.auto.common.driver.ZetaOSXDriver;
 import java.util.concurrent.Future;
 
-import com.wearezeta.auto.osx.locators.OSXLocators;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 public class ContactContextMenuPage extends OSXPage {
 
 	private static final int CONTEXT_ARCHIVE_INDEX = 1;
-	private static final int CONTEXT_SILENCEI_INDEX = 2;
+	private static final int CONTEXT_SILENCE_INDEX = 2;
 	private static final int CONTEXT_DELETE_INDEX = 3;
 	private static final int CONTEXT_BLOCK_INDEX = 4;
 
 	// TODO hide behind driver impl
 	private final Robot robot = new Robot();
-
-	@FindBy(xpath = OSXLocators.ContactListContextMenuPage.xpathContextBlock)
-	private WebElement contextBlock;
 
 	public ContactContextMenuPage(Future<ZetaOSXDriver> lazyDriver)
 			throws Exception {
@@ -38,11 +32,12 @@ public class ContactContextMenuPage extends OSXPage {
 	}
 
 	public void clickSilence() throws Exception {
-		selectByIndex(CONTEXT_SILENCEI_INDEX, 1000);
+		// https://www.youtube.com/watch?v=XFCU0dYgeeQ
+		selectByIndex(CONTEXT_SILENCE_INDEX, 1000);
 	}
 
 	public void clickNotify() throws Exception {
-		selectByIndex(CONTEXT_SILENCEI_INDEX, 1000);
+		selectByIndex(CONTEXT_SILENCE_INDEX, 1000);
 	}
 
 	public void clickDelete() throws Exception {
