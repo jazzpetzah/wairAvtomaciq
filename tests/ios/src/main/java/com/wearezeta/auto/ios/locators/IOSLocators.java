4d8a1f23b5dc9693f938fa58d615e8aea48ea959
Piotr <piotr@wire.com>
2015-10-02 14:01:56 +0300

    added 2 tests for iOS and fixes

--- tests/ios/src/main/java/com/wearezeta/auto/ios/locators/IOSLocators.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/locators/IOSLocators.java
@@ -644,6 +644,8 @@ public final class IOSLocators {
 		public static final String xpathMessageEntries = "//UIAWindow[@name='ZClientMainWindow']/UIATableView[1]/UIATableCell";
 
 		public static final String xpathSimpleMessageLink = "//UIAApplication[1]/UIAWindow[@name='ZClientMainWindow']/UIATableView[1]/UIATableCell[last()]/UIATextView[1]";
+		
+		public static final String xpathConnectedToUserLabel = "//UIAStaticText[contains(@name, 'CONNECTED TO %s')]";
 	}
 
 	public final class DialogInfoPage {
