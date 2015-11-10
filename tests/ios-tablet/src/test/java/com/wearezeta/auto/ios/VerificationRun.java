dd6fd09c2ba6f7dfa3df253c1acee5e56b60ea5a
Piotr <piotr@wire.com>
2015-11-10 17:05:47 +0200

    iOS fix obsolete tests to not be rub with verification run

--- tests/ios-tablet/src/test/java/com/wearezeta/auto/ios/VerificationRun.java
+++ tests/ios-tablet/src/test/java/com/wearezeta/auto/ios/VerificationRun.java
@@ -8,7 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources", format = {
 		"html:target/report", "json:target/report.json",
-		"com.wearezeta.auto.ios.tools.IOSZetaFormatter" }, tags = { "@mute" })
+		"com.wearezeta.auto.ios.tools.IOSZetaFormatter" }, tags = { "@mute",
+		"~@obsolete" })
 public class VerificationRun {
 
 }
