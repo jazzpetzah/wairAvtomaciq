5de95ad9176f709af665809099ae087576a980c4
Piotr <piotr@wire.com>
2015-11-25 14:26:22 +0200

    iOS: tags to exclude obsolete tests tone run with staging

--- tests/ios/src/test/java/com/wearezeta/auto/ios/StagingRun.java
+++ tests/ios/src/test/java/com/wearezeta/auto/ios/StagingRun.java
@@ -7,7 +7,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(format = { "html:target/report", "json:target/report.json",
-		"com.wearezeta.auto.ios.tools.IOSZetaFormatter" }, tags = { "@staging" })
+		"com.wearezeta.auto.ios.tools.IOSZetaFormatter" }, tags = { "@staging",
+		"~@obsolete" })
 public class StagingRun {
 
 }
