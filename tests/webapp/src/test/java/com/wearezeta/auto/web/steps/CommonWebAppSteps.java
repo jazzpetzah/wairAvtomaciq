518e382b31e5c0d1180528685eff250f6de90f19
Michael Koppen <michael.koppen@googlemail.com>
2015-11-26 11:37:16 +0100

    Webapp: Possible fix for multiple tabs in safari issue

--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
@@ -172,6 +172,12 @@ public class CommonWebAppSteps {
 						capabilities);
 				// setup of the browser
 				lazyWebDriver.setFileDetector(new LocalFileDetector());
+				if (WebAppExecutionContext.getBrowser().equals(Browser.Safari)) {
+					WebCommonUtils.closeAllAdditionalTabsInSafari(lazyWebDriver
+							.getNodeIp());
+					WebCommonUtils.clearHistoryInSafari(lazyWebDriver
+							.getNodeIp());
+				}
 				if (WebAppExecutionContext.getBrowser()
 						.isSupportingMaximizingTheWindow()) {
 					lazyWebDriver.manage().window().maximize();
@@ -187,12 +193,6 @@ public class CommonWebAppSteps {
 												WebAppConstants.MIN_WEBAPP_WINDOW_WIDTH,
 												WebAppConstants.MIN_WEBAPP_WINDOW_HEIGHT));
 					}
-				}
-				if (WebAppExecutionContext.getBrowser().equals(Browser.Safari)) {
-					WebCommonUtils.closeAllAdditionalTabsInSafari(lazyWebDriver
-							.getNodeIp());
-					WebCommonUtils.clearHistoryInSafari(lazyWebDriver
-							.getNodeIp());
 				}
 				return lazyWebDriver;
 			}
