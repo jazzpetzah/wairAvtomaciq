8906d0b8eced2635c20b0216e5db7d59601d496a
Sven Jost <sven@wire.com>
2016-01-04 12:16:00 +0100

    Webapp: Fix for step 'I see Sign in page'

--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/LoginPageSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/LoginPageSteps.java
@@ -204,7 +204,7 @@ public class LoginPageSteps {
 	@Given("^I see Sign In page$")
 	public void ISeeSignInPage() throws Exception {
 		Assert.assertTrue(webappPagesCollection.getPage(LoginPage.class)
-				.isVisible());
+				.isSignInFormVisible());
 	}
 
 	/**
