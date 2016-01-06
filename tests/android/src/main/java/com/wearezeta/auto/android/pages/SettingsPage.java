e2768c3a241b78aa55579a976c1fb6407c874818
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-01-06 10:30:17 +0100

    Remove obsolete test (now the theme cannot be changes from settings)

--- tests/android/src/main/java/com/wearezeta/auto/android/pages/SettingsPage.java
+++ tests/android/src/main/java/com/wearezeta/auto/android/pages/SettingsPage.java
@@ -1,7 +1,5 @@
 package com.wearezeta.auto.android.pages;
 
-import java.awt.image.BufferedImage;
-import java.util.Optional;
 import java.util.concurrent.Future;
 import java.util.function.Function;
 
@@ -15,12 +13,6 @@ public class SettingsPage extends AndroidPage {
 
     private static final Function<String, String> xpathSettingsMenuItemByText = text -> String
             .format("//*[@id='title' and @value='%s']", text);
-
-    private static final Function<String, String> xpathSettingsMenuItemByPartOfText = text -> String
-            .format("//*[@id='title' and contains(@value, '%s')]", text);
-
-    private static final String xpathThemeSwitch = String
-            .format("%s/parent::*/parent::*//*[@id='switchWidget']", xpathSettingsMenuItemByPartOfText.apply("Theme"));
 
     private static final Function<String, String> xpathConfirmBtnByName = name -> String
             .format("//*[starts-with(@id, 'button') and @value='%s']", name);
@@ -63,18 +55,5 @@ public class SettingsPage extends AndroidPage {
         assert DriverUtils.waitUntilLocatorIsDisplayed(getDriver(), locator) :
                 "Log out confirmation is not visible";
         getDriver().findElement(locator).click();
-    }
-
-    public Optional<BufferedImage> getThemeSwitcherState() throws Exception {
-        final By itemLocator = By.xpath(xpathSettingsMenuItemByPartOfText.apply(" Theme"));
-        assert scrollUntilMenuElementVisible(itemLocator, 5) : "Theme menu item is not visible";
-        return this.getElementScreenshot(getDriver().findElement(By.xpath(xpathThemeSwitch)));
-    }
-
-    public void switchTheme() throws Exception {
-        final By switchLocator = By.xpath(xpathThemeSwitch);
-        assert scrollUntilMenuElementVisible(By.xpath(xpathSettingsMenuItemByPartOfText.apply(" Theme")), 5)
-                : "Theme menu item is not visible";
-        getDriver().findElement(switchLocator).click();
     }
 }
