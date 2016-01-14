95cd4de0c0aeb9e2b0691c7cb13c9ad542ec94bf
Sven Jost <sven@wire.com>
2016-01-14 15:52:08 +0100

    Wrapper OSX: fix import

--- tests/macosx/src/test/java/com/wearezeta/auto/osx/steps/webapp/RegistrationPageSteps.java
+++ tests/macosx/src/test/java/com/wearezeta/auto/osx/steps/webapp/RegistrationPageSteps.java
@@ -29,7 +29,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class RegistrationPageSteps {
 
