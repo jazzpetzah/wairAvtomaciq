13200c9fd9902c12423484320c4ce62517c716bc
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-01-27 10:54:08 +0100

    fix taking a picture if device orientation is corrupted

--- tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/common/ScreenOrientationHelper.java
+++ tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/common/ScreenOrientationHelper.java
@@ -10,42 +10,40 @@ import com.wearezeta.auto.common.driver.ZetaAndroidDriver;
  * This class is a dirty fix for App/Selendroid(?) issue The problem is that
  * device orientation is sometimes overriden without any reason and we have to
  * fix it in such a tricky way
- * 
- * @author elf
  *
+ * @author elf
  */
 public final class ScreenOrientationHelper {
 
-	private static ScreenOrientationHelper instance;
+    private static ScreenOrientationHelper instance;
 
-	public static synchronized ScreenOrientationHelper getInstance() {
-		if (instance == null) {
-			instance = new ScreenOrientationHelper();
-		}
-		return instance;
-	}
+    public static synchronized ScreenOrientationHelper getInstance() {
+        if (instance == null) {
+            instance = new ScreenOrientationHelper();
+        }
+        return instance;
+    }
 
-	private ScreenOrientationHelper() {
-	}
+    private ScreenOrientationHelper() {
+    }
 
-	private Optional<ScreenOrientation> orientation = Optional.empty();
+    private Optional<ScreenOrientation> orientation = Optional.empty();
 
-	public void setOrientation(ScreenOrientation orientation) {
-		this.orientation = Optional.of(orientation);
-	}
+    public void setOrientation(ScreenOrientation orientation) {
+        this.orientation = Optional.of(orientation);
+    }
 
-	public void resetOrientation() {
-		this.orientation = Optional.empty();
-	}
+    public void resetOrientation() {
+        this.orientation = Optional.empty();
+    }
 
-	public ScreenOrientation fixOrientation(final ZetaAndroidDriver driver)
-			throws InterruptedException {
-		if (this.orientation.isPresent()) {
-			driver.rotate(orientation.get());
-			Thread.sleep(500);
-			return this.orientation.get();
-		} else {
-			return driver.getOrientation();
-		}
-	}
+    public ScreenOrientation fixOrientation(final ZetaAndroidDriver driver) throws InterruptedException {
+        if (this.orientation.isPresent()) {
+            driver.rotate(orientation.get());
+            Thread.sleep(500);
+            return this.orientation.get();
+        } else {
+            return driver.getOrientation();
+        }
+    }
 }
