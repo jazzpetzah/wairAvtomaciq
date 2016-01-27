94a605def3b3528007e82951e7438c50dd3d1fbc
Sven Jost <sven@wire.com>
2016-01-27 16:45:56 +0100

    Webapp: Fix order of steps

--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/LoginPageSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/LoginPageSteps.java
@@ -60,8 +60,8 @@ public class LoginPageSteps {
 				+ password);
 		this.IEnterEmail(login);
 		this.IEnterPassword(password);
-		this.IPressSignInButton();
 		this.ICheckOptionToRememberMe(null);
+		this.IPressSignInButton();
 	}
 
 	/**
