70f4975af8aabce6469a649f1469c571214c48e9
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-01-29 13:53:22 +0100

    Update confirmation buttons locators

--- tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/popovers/AbstractParticipantPage.java
+++ tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/popovers/AbstractParticipantPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import com.wearezeta.auto.common.driver.ZetaAndroidDriver;
 
 abstract class AbstractParticipantPage extends AbstractPopoverPage {
-	public static final By xpathConfirmRemoveButton = By.xpath("//*[@id='confirm' and @value='REMOVE']");
+	public static final By xpathConfirmRemoveButton = By.xpath("//*[@id='positive' and @value='REMOVE']");
 
 	public static final By xpathRemoveButton =
 			By.xpath("//*[@id='fl__participant__tab__container']//*[@id='gtv__participants__right__action']");
--- tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/popovers/ParticipantsPage.java
+++ tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/popovers/ParticipantsPage.java
@@ -3,7 +3,6 @@ package com.wearezeta.auto.android_tablet.pages.popovers;
 import java.util.concurrent.Future;
 import java.util.function.Function;
 
-import com.wearezeta.auto.android.pages.OtherUserPersonalInfoPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -11,7 +10,7 @@ import com.wearezeta.auto.common.driver.DriverUtils;
 import com.wearezeta.auto.common.driver.ZetaAndroidDriver;
 
 public class ParticipantsPage extends AbstractConversationDetailsPage {
-    public static final By xpathConfirmLeaveButton = By.xpath("//*[@id='confirm' and @value='LEAVE']");
+    public static final By xpathConfirmLeaveButton = By.xpath("//*[@id='positive' and @value='LEAVE']");
 
     public static final Function<String, String> xpathStrParticipantAvatarByName = name -> String
             .format("//*[@id='pfac__participants']//*[@id='ttv__group__adapter' and @value='%s']/parent::*/parent::*",
@@ -25,13 +24,8 @@ public class ParticipantsPage extends AbstractConversationDetailsPage {
     private static final Function<String, String> xpathStrSubheaderByValue = value -> String
             .format("//*[@id='ttv__participants__sub_header' and @value='%s']", value);
 
-    public ParticipantsPage(Future<ZetaAndroidDriver> lazyDriver,
-                            GroupPopover container) throws Exception {
+    public ParticipantsPage(Future<ZetaAndroidDriver> lazyDriver, GroupPopover container) throws Exception {
         super(lazyDriver, container);
-    }
-
-    private OtherUserPersonalInfoPage getOUPIPageInstance() throws Exception {
-        return this.getAndroidPageInstance(OtherUserPersonalInfoPage.class);
     }
 
     public void tapConfirmLeaveButton() throws Exception {
@@ -49,8 +43,7 @@ public class ParticipantsPage extends AbstractConversationDetailsPage {
         getElement(locator, String.format("The avatar of '%s' is not visible", name)).click();
     }
 
-    public boolean waitForParticipantAvatarNotVisible(String name)
-            throws Exception {
+    public boolean waitForParticipantAvatarNotVisible(String name) throws Exception {
         final By locator = By.xpath(xpathStrParticipantAvatarByName.apply(name));
         return DriverUtils.waitUntilLocatorDissapears(getDriver(), locator);
     }
@@ -63,18 +56,12 @@ public class ParticipantsPage extends AbstractConversationDetailsPage {
         getDriver().tapSendButton();
     }
 
-    public void tapOpenConversationButton() throws Exception {
-        getOUPIPageInstance().tapLeftActionBtn();
-    }
-
-    public boolean waitUntilConversationNameVisible(String expectedName)
-            throws Exception {
+    public boolean waitUntilConversationNameVisible(String expectedName) throws Exception {
         final By locator = By.xpath(xpathConvoNameInputByValue.apply(expectedName));
         return DriverUtils.waitUntilLocatorIsDisplayed(getDriver(), locator);
     }
 
-    public boolean waitUntilSubheaderIsVisible(String expectedText)
-            throws Exception {
+    public boolean waitUntilSubheaderIsVisible(String expectedText) throws Exception {
         final By locator = By.xpath(xpathStrSubheaderByValue.apply(expectedText));
         return DriverUtils.waitUntilLocatorIsDisplayed(getDriver(), locator);
     }
