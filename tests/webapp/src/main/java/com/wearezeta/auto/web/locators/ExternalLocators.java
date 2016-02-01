e53d7c5c68a3a550921eab2e5864c9721cabd7a8
Jann-Niklas Zimmermann <jann-niklas.zimmermann@wire.com>
2016-02-01 14:32:33 +0100

    Add test scenarios for the verify page Add VerifyPageSteps and VerifyPage with methods for the tests Add new external locators for the verify page

--- tests/webapp/src/main/java/com/wearezeta/auto/web/locators/ExternalLocators.java
+++ tests/webapp/src/main/java/com/wearezeta/auto/web/locators/ExternalLocators.java
@@ -38,5 +38,16 @@ public final class ExternalLocators {
 		public final static String cssDownloadWindows = "[data-ga-category='download'][data-ga-action='download'][data-ga-value='windows']";
 		public final static String cssDownloadWebapp = "[data-ga-category='download'][data-ga-action='download'][data-ga-value='webapp']";
 	}
+	
+	public static final class VerifyPage {
+		public final static String cssDownloadIPhone = "[data-ga-category='verify'][data-ga-action='download'][data-ga-value='ios']";
+		public final static String cssDownloadAndroid = "[data-ga-category='verify'][data-ga-action='download'][data-ga-value='android']";
+		public final static String cssDownloadOSX = "a[href*='mt=12']";
+		public final static String cssDownloadWindows = "a[href*='download']";
+		public final static String cssWebappButton = "a[href*='webapp']";
+		
+		public final static Function<String, String> xpathLabelByText = txt -> String
+				.format("//*[contains(text(),'%s')]", txt);
+	}
 
 }
