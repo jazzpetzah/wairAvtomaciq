33385e6421c02cdec11f07a4eeed962b250cef77
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-02-02 15:47:43 +0100

    Fix runner for tablet

--- tests/ios-tablet/src/test/java/com/wearezeta/auto/ios/RCRun.java
+++ tests/ios-tablet/src/test/java/com/wearezeta/auto/ios/RCRun.java
@@ -10,7 +10,7 @@ import com.wearezeta.auto.common.rc.RCTestcase;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources", format = {
 		"html:target/report", "json:target/report.json",
-		"ccom.wearezeta.auto.common.ZetaFormatterr", "rerun:target/rerun.txt" }, tags = { RCTestcase.RC_TAG })
+		"com.wearezeta.auto.common.ZetaFormatter", "rerun:target/rerun.txt" }, tags = { RCTestcase.RC_TAG })
 public class RCRun {
 
 }
