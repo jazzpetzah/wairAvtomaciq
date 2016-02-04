ff08943c4af80d1c824666a998529e0233c723de
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-02-04 16:48:30 +0100

    Fix sketching in Simulator

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/SketchPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/SketchPage.java
@@ -3,30 +3,38 @@ package com.wearezeta.auto.ios.pages;
 import java.util.Random;
 import java.util.concurrent.Future;
 
+import com.wearezeta.auto.common.CommonUtils;
+import com.wearezeta.auto.ios.tools.IOSSimulatorHelper;
 import org.openqa.selenium.By;
 
 import com.wearezeta.auto.common.driver.ZetaIOSDriver;
 import com.wearezeta.auto.common.driver.DriverUtils;
 
-public class SketchPage extends IOSPage{
-	private static final By nameSketchSendButton = By.name("SketchConfirmButton");
+public class SketchPage extends IOSPage {
+    private static final By nameSketchSendButton = By.name("SketchConfirmButton");
 
-	public SketchPage(Future<ZetaIOSDriver> driver) throws Exception {
-		super(driver);
-	}
-	
-	public void sketchRandomLines() throws Exception {
-		for (int i=0; i<5; i++) {
-			Random random = new Random();
-			int startX = random.nextInt(100);
-			int endX = random.nextInt(100);
-			int endY = random.nextInt(100);
-			DriverUtils.swipeByCoordinates(getDriver(), 500, startX, 50, endX, endY);
-		}
-	}
-	
-	public void sendSketch() throws Exception {
-		getElement(nameSketchSendButton).click();
-	}
+    public SketchPage(Future<ZetaIOSDriver> driver) throws Exception {
+        super(driver);
+    }
+
+    private static final Random rand = new Random();
+
+    public void sketchRandomLines() throws Exception {
+        for (int i = 0; i < 2; i++) {
+            int startX = 5 + rand.nextInt(90);
+            int endX = 5 + rand.nextInt(90);
+            int startY = 30 + rand.nextInt(40);
+            int endY = 30 + rand.nextInt(40);
+            if (CommonUtils.getIsSimulatorFromConfig(getClass())) {
+                IOSSimulatorHelper.swipe(startX / 100.0, startY / 100.0, endX / 100.0, endY / 100.0);
+            } else {
+                DriverUtils.swipeByCoordinates(getDriver(), 500, startX, startY, endX, endY);
+            }
+        }
+    }
+
+    public void sendSketch() throws Exception {
+        getElement(nameSketchSendButton).click();
+    }
 
 }
