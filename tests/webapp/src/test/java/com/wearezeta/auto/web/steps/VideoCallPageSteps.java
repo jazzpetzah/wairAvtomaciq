3702f79e8a174d01038ea9c3da604e9fdb5bf234
Anastasia <anastasiia.gerasymenko@wearezeta.com>
2016-02-08 11:08:43 +0100

    formatting

--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/VideoCallPageSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/VideoCallPageSteps.java
@@ -1,18 +1,13 @@
 package com.wearezeta.auto.web.steps;
 
-import com.wearezeta.auto.common.log.ZetaLogger;
-import com.wearezeta.auto.common.usrmgmt.ClientUsersManager;
 import com.wearezeta.auto.web.pages.VideoCallPage;
 import com.wearezeta.auto.web.pages.WebappPagesCollection;
 import cucumber.api.java.en.When;
-import org.apache.log4j.Logger;
 import org.junit.Assert;
 
 public class VideoCallPageSteps {
 
     private final WebappPagesCollection webappPagesCollection = WebappPagesCollection.getInstance();
-
-    private static final Logger log = ZetaLogger.getLog(VideoCallPageSteps.class.getSimpleName());
 
     @When("^I end the video call$")
     public void IEndTheCall() throws Exception {
