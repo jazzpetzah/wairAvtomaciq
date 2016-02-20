08b18c259917f7b8d31f7729d582abf507b4dc0c
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-02-20 22:19:46 +0100

    Wait longer for settings warning to appear

--- tests/ios-tablet/src/test/java/com/wearezeta/auto/ios/steps/TabletLoginPageSteps.java
+++ tests/ios-tablet/src/test/java/com/wearezeta/auto/ios/steps/TabletLoginPageSteps.java
@@ -47,6 +47,6 @@ public class TabletLoginPageSteps {
         } catch (WebDriverException e) {
             // pass silently
         }
-        getTabletLoginPage().dismissSettingsWarningIfVisible(5);
+        getTabletLoginPage().dismissSettingsWarningIfVisible(10);
     }
 }
