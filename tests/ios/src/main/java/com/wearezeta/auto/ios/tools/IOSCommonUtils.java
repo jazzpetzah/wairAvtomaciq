05cb8a656b9710792597c909127774b800d8f682
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-02-24 13:56:10 +0100

    Tune iOS logging

--- tests/ios/src/main/java/com/wearezeta/auto/ios/tools/IOSCommonUtils.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/tools/IOSCommonUtils.java
@@ -1,11 +1,9 @@
 package com.wearezeta.auto.ios.tools;
 
-import java.awt.Toolkit;
-import java.awt.datatransfer.StringSelection;
 import java.io.File;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
 
 import org.apache.log4j.Logger;
 
@@ -18,27 +16,22 @@ import com.wearezeta.auto.common.misc.ClientDeviceInfo;
 import com.wearezeta.auto.ios.pages.IOSPage;
 
 public class IOSCommonUtils {
-    private static Logger log = ZetaLogger.getLog(IOSCommonUtils.class
-            .getSimpleName());
+    private static Logger log = ZetaLogger.getLog(IOSCommonUtils.class.getSimpleName());
 
     public static BuildVersionInfo readClientVersionFromPlist() {
         String clientBuild = "no info";
         String zmessagingBuild = "no info";
         try {
-            File file = new File(
-                    getIosClientInfoPlistFromConfig(IOSCommonUtils.class));
-            NSDictionary rootDict = (NSDictionary) PropertyListParser
-                    .parse(file);
+            File file = new File(getIosClientInfoPlistFromConfig(IOSCommonUtils.class));
+            NSDictionary rootDict = (NSDictionary) PropertyListParser.parse(file);
             clientBuild = rootDict.objectForKey("CFBundleVersion").toString();
             String trackInfo = rootDict.objectForKey("WireBundleId").toString();
             String majorVersion = rootDict.objectForKey("CFBundleShortVersionString").toString();
             trackInfo = trackInfo.substring(trackInfo.indexOf("-") + 1, trackInfo.length());
             clientBuild = majorVersion + "." + clientBuild + "-" + trackInfo;
             log.info("Got build number: " + clientBuild);
-            NSDictionary zcBuildInfo = (NSDictionary) rootDict
-                    .objectForKey("ZCBuildInfo");
-            NSDictionary zmessagingDict = (NSDictionary) zcBuildInfo
-                    .objectForKey("zmessaging");
+            NSDictionary zcBuildInfo = (NSDictionary) rootDict.objectForKey("ZCBuildInfo");
+            NSDictionary zmessagingDict = (NSDictionary) zcBuildInfo.objectForKey("zmessaging");
             zmessagingBuild = zmessagingDict.objectForKey("version").toString();
         } catch (Exception ex) {
             log.error("Failed to read iOS client properties.\n"
@@ -49,18 +42,15 @@ public class IOSCommonUtils {
 
     public static ClientDeviceInfo readDeviceInfo() throws Exception {
         String os = "iOS";
-        String osBuild = (String) IOSPage
-                .executeScript("UIATarget.localTarget().systemVersion();");
-        String deviceName = (String) IOSPage
-                .executeScript("UIATarget.localTarget().model();");
+        String osBuild = (String) IOSPage.executeScript("UIATarget.localTarget().systemVersion();");
+        String deviceName = (String) IOSPage.executeScript("UIATarget.localTarget().model();");
         String gsmNetworkType = "";
         Boolean isWifiEnabled = true;
 
         return new ClientDeviceInfo(os, osBuild, deviceName, gsmNetworkType, isWifiEnabled);
     }
 
-    public static String getIosClientInfoPlistFromConfig(Class<?> c)
-            throws Exception {
+    public static String getIosClientInfoPlistFromConfig(Class<?> c) throws Exception {
         return CommonUtils.getValueFromConfig(c, "iosClientInfoPlist");
     }
 
@@ -74,19 +64,17 @@ public class IOSCommonUtils {
      */
     private static String getConnectediOSDeviceUDID(String deviceName,
                                                     boolean shouldThrowException) throws Exception {
-        final String result = CommonUtils
-                .executeOsXCommandWithOutput(new String[]{
-                        "/bin/bash",
-                        "-c",
-                        "system_profiler SPUSBDataType | sed -n '/"
-                                + deviceName
-                                + "/,/Serial/p' | grep 'Serial Number:' | awk -F ': ' '{print $2}'"});
+        final String result = CommonUtils.executeOsXCommandWithOutput(new String[]{
+                "/bin/bash",
+                "-c",
+                "system_profiler SPUSBDataType | sed -n '/"
+                        + deviceName
+                        + "/,/Serial/p' | grep 'Serial Number:' | awk -F ': ' '{print $2}'"});
         if (result.length() > 0) {
             return result.trim();
         } else {
             if (shouldThrowException) {
-                throw new RuntimeException(
-                        "Cannot detect any connected iPhone device");
+                throw new RuntimeException("Cannot detect any connected iPhone device");
             } else {
                 return null;
             }
@@ -96,24 +84,6 @@ public class IOSCommonUtils {
     public static String getConnectediPhoneUDID(boolean shouldThrowException)
             throws Exception {
         return getConnectediOSDeviceUDID("iPhone", shouldThrowException);
-    }
-
-    public static void copyToSystemClipboard(String text) {
-        StringSelection str = new StringSelection(text);
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);
-    }
-
-    public static long stringToTime(String time) {
-        long resultTime = 0;
-        SimpleDateFormat sdf = new SimpleDateFormat(
-                "EEEE, MMMM d, yyyy '∙' h:mm a");
-        try {
-            Date date = sdf.parse(time);
-            resultTime = date.getTime();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return resultTime;
     }
 
     private static String getIOSSimulatorIdByDeviceName(String deviceName) throws Exception {
@@ -128,27 +98,21 @@ public class IOSCommonUtils {
                 .trim();
     }
 
-    public static void collectSimulatorLogs(String deviceName,
-                                            Date testStartedDate) throws Exception {
+    public static void collectSimulatorLogs(String deviceName) throws Exception {
         log.debug("iOS Simulator Logs:");
         final String simId = getIOSSimulatorIdByDeviceName(deviceName);
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        final String logStartTime = sdf.format(testStartedDate);
-        final String logEndTime = sdf.format(new Date());
-        final String collectedLogEntries = CommonUtils
-                .executeOsXCommandWithOutput(new String[]{
-                        "/bin/bash",
-                        "-c",
-                        String.format(
-                                "awk -v start=%s -v stop=%s 'start <= $3 && $3 < stop' $HOME/Library/Logs/CoreSimulator/%s/system.log"
-                                        + " | grep -Ei '(wire|zclient|CoreSimulator)'",
-                                logStartTime, logEndTime, simId)});
-        log.debug(collectedLogEntries);
+        final File logFile = new File(String.format("%s/Library/Logs/CoreSimulator/%s/system.log",
+                System.getProperty("user.home"), simId));
+        if (logFile.exists()) {
+            log.debug(new String(Files.readAllBytes(logFile.toPath()), Charset.forName("UTF-8")) +
+                    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        } else {
+            log.error(String.format("There is no log file at the expected path %s\n\n\n\n\n",
+                    logFile.getCanonicalPath()));
+        }
     }
 
-    public static String getPerfReportPathFromConfig(Class<?> c)
-            throws Exception {
+    public static String getPerfReportPathFromConfig(Class<?> c) throws Exception {
         return CommonUtils.getValueFromConfig(c, "perfReportPath");
     }
-
 }
