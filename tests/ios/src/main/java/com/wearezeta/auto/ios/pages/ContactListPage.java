a77121dbad6bd0aabaf22f8c5c177af8747c0e36
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-03-09 10:23:27 +0100

    Decrease wait timeout

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ContactListPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ContactListPage.java
@@ -73,7 +73,7 @@ public class ContactListPage extends IOSPage {
     public void openSearch() throws Exception {
         getElement(nameOpenStartUI).click();
         // Wait until animation is completed
-        DriverUtils.waitUntilLocatorIsDisplayed(getDriver(), PeoplePickerPage.xpathPickerClearButton);
+        DriverUtils.waitUntilLocatorIsDisplayed(getDriver(), PeoplePickerPage.xpathPickerClearButton, 3);
     }
 
     public boolean isPlayPauseButtonVisible(String contact) throws Exception {
