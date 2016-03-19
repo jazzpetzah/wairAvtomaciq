51d7052e9fab13d3b59df8c6a64103ea0adf6756
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-03-19 08:49:59 +0100

    Tune process search

--- tests/common/src/main/java/com/wearezeta/common/process/UnixProcessHelpers.java
+++ tests/common/src/main/java/com/wearezeta/common/process/UnixProcessHelpers.java
@@ -24,19 +24,23 @@ public class UnixProcessHelpers {
         new ProcessBuilder(ArrayUtils.addAll(new String[]{"/usr/bin/killall"}, expectedNames)).start().waitFor();
         final long millisecondsStarted = System.currentTimeMillis();
         boolean areAllProcessesTerminated = false;
-        Set<String> runningProcesses = new HashSet<>();
         while (System.currentTimeMillis() - millisecondsStarted <= timeoutSecondsUntilForceKill * 1000) {
             final Process p = new ProcessBuilder("/bin/ps", "axco", "command").start();
             final InputStream stream = p.getInputStream();
             final BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
             String line;
-            runningProcesses.clear();
             while ((line = reader.readLine()) != null) {
-                runningProcesses.add(line);
+                for (String name : expectedNames) {
+                    if (line.matches(".*\\b" + name.trim() + "\\b.*")) {
+                        areAllProcessesTerminated = true;
+                        break;
+                    }
+                }
+                if (areAllProcessesTerminated) {
+                    break;
+                }
             }
-            runningProcesses.retainAll(new HashSet<>(Arrays.asList(expectedNames)));
-            if (runningProcesses.isEmpty()) {
-                areAllProcessesTerminated = true;
+            if (areAllProcessesTerminated) {
                 break;
             } else {
                 Thread.sleep(100);
@@ -44,7 +48,7 @@ public class UnixProcessHelpers {
         }
         if (!areAllProcessesTerminated) {
             log.warn(String.format("Not all processes managed to stop gracefully in %s seconds. Force killing of %s...",
-                    timeoutSecondsUntilForceKill, runningProcesses));
+                    timeoutSecondsUntilForceKill, Arrays.toString(expectedNames)));
             new ProcessBuilder(ArrayUtils.addAll(new String[]{"/usr/bin/killall", "-9"}, expectedNames)).
                     start().waitFor(1, TimeUnit.SECONDS);
         }
