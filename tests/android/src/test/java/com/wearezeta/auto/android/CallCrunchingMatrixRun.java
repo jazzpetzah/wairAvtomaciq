7e30ba31f8ff8f872c17e21f8e44b27f0d10e31c
Robin <robin@wire.com>
2016-04-01 14:26:17 +0200

    fix cucumber

--- tests/android/src/test/java/com/wearezeta/auto/android/CallCrunchingMatrixRun.java
+++ tests/android/src/test/java/com/wearezeta/auto/android/CallCrunchingMatrixRun.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty", "html:target/report", "json:target/test_report.json",
+@CucumberOptions(format = {"html:target/report", "json:target/test_report.json",
         "com.wearezeta.auto.common.ZetaFormatter"}, tags = {"@calling_matrix"})
 
 public class CallCrunchingMatrixRun {
