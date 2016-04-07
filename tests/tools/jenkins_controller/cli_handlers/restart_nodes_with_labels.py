327312a758578c00194cca92b7df4f8d928d26fc
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-04-07 17:39:39 +0200

    Skip nodes where we cannot read hostname

--- tests/tools/jenkins_controller/cli_handlers/restart_nodes_with_labels.py
+++ tests/tools/jenkins_controller/cli_handlers/restart_nodes_with_labels.py
@@ -5,6 +5,7 @@ import os
 import paramiko
 import sys
 import time
+import xml.etree.ElementTree as ET
 
 from cli_handlers.cli_handler_base import CliHandlerBase
 
@@ -69,16 +70,23 @@ class RestartNodesWithLabels(CliHandlerBase):
         count_of_nodes_to_restart = 0
         workers = []
         for _, node in self._jenkins.get_nodes().iteritems():
-            hostname = node.nodename
-            if not self._is_alive(hostname):
-                sys.stderr.write('The host {} seems to be already offline\n'.format(hostname))
-                continue
-            node_labels_str = node.get_labels()
+            response = node.jenkins.requester.get_and_confirm_status("%(baseurl)s/config.xml" % node.__dict__)
+            sys.stderr.write(response.text)
+            et = ET.fromstring(response.text)
+            node_labels_str = et.find('label').text
             if node_labels_str:
                 node_labels = self._normalize_labels(node_labels_str.split(' '))
             else:
                 node_labels = set()
             if expected_labels.issubset(node_labels):
+                try:
+                    hostname = et.find('.//host').text
+                except Exception:
+                    sys.stderr.write('Cannot read hostname property of "{}" node! Skipping it...\n'.format(node.name))
+                    continue
+                if not self._is_alive(hostname):
+                    sys.stderr.write('The host {} seems to be already offline\n'.format(hostname))
+                    continue
                 sys.stderr.write('Found matching node "{}". Restarting...\n'.format(node.name))
                 workers.append(Process(target=self._restart_node_and_wait,
                                        args=(node, hostname, args.node_user, args.node_password)))
