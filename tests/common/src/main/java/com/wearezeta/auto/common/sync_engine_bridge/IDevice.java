b68a04ca8292fab0246ffa0c8621b017efae0ad4
Sven Jost <sven@wire.com>
2016-04-12 18:05:27 +0200

    Common: Fix some indentation issues

--- tests/common/src/main/java/com/wearezeta/auto/common/sync_engine_bridge/IDevice.java
+++ tests/common/src/main/java/com/wearezeta/auto/common/sync_engine_bridge/IDevice.java
@@ -5,25 +5,26 @@ import com.wearezeta.auto.common.usrmgmt.ClientUser;
 import java.util.Optional;
 
 public interface IDevice extends IRemoteEntity {
-	boolean hasLoggedInUser();
 
-	boolean isLoggedInUser(ClientUser user);
+    boolean hasLoggedInUser();
+
+    boolean isLoggedInUser(ClientUser user);
 
     Optional<ClientUser> getLoggedInUser();
 
     void logInWithUser(ClientUser user) throws Exception;
 
-	void sendMessage(String convId, String message) throws Exception;
+    void sendMessage(String convId, String message) throws Exception;
 
-	void sendImage(String convId, String path) throws Exception;
+    void sendImage(String convId, String path) throws Exception;
 
-	void sendPing(String convId) throws Exception;
+    void sendPing(String convId) throws Exception;
 
-	void clearConversation(String convId) throws Exception;
+    void clearConversation(String convId) throws Exception;
 
-	// TODO: void sendAsset(String convId, byte[] data, String mime, String filename) throws Exception;
+    // TODO: void sendAsset(String convId, byte[] data, String mime, String filename) throws Exception;
 
-	void sendFile(String convId, String path, String mime) throws Exception;
+    void sendFile(String convId, String path, String mime) throws Exception;
 
     void setLabel(String label) throws Exception;
 
@@ -31,5 +32,5 @@ public interface IDevice extends IRemoteEntity {
 
     String getFingerprint() throws Exception;
 
-	void destroy();
+    void destroy();
 }
