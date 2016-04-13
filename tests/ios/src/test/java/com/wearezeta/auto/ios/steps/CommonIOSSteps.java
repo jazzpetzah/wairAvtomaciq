e507442a67404a600f6f3befaa4c3e16acefec0b
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-04-13 15:53:11 +0200

    Automate iOS regression test # C95627

--- tests/ios/src/test/java/com/wearezeta/auto/ios/steps/CommonIOSSteps.java
+++ tests/ios/src/test/java/com/wearezeta/auto/ios/steps/CommonIOSSteps.java
@@ -960,4 +960,21 @@ public class CommonIOSSteps {
     public void IConfirmImageSelection() throws Exception {
         pagesCollection.getCommonPage().pressConfirmButton();
     }
+
+    /**
+     * Execute Delete Conversation action on the particular device registered for this user
+     *
+     * @step. ^User (.*) deletes? (single user|group) conversation (.*) using device (.*)
+     *
+     * @param userAs user name/alias
+     * @param convoType either 'group' or 'single user'
+     * @param convoName conversation name
+     * @param deviceName device name (this one should already exist)
+     * @throws Exception
+     */
+    @Given("^User (.*) deletes? (single user|group) conversation (.*) using device (.*)")
+    public void UserDeletedConversation(String userAs, String convoType, String convoName, String deviceName)
+            throws Exception {
+        commonSteps.UserClearsConversation(userAs, convoName, deviceName, convoType.equals("group"));
+    }
 }
