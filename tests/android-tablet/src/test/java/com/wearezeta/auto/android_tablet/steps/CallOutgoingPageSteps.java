07121535464c5b6ea73a947412090b39b9e68e60
Robin <robin@wire.com>
2016-04-15 10:15:33 +0200

    also update the calloutgoing page from tablet

--- tests/android-tablet/src/test/java/com/wearezeta/auto/android_tablet/steps/CallOutgoingPageSteps.java
+++ tests/android-tablet/src/test/java/com/wearezeta/auto/android_tablet/steps/CallOutgoingPageSteps.java
@@ -37,15 +37,18 @@ public class CallOutgoingPageSteps {
     /**
      * Verifies presence of outgoing call
      *
+     * @param not
+     * @param videoCall not null when it is outgoing video call
      * @throws Exception
-     * @step. ^I (do not )?see outgoing call$
+     * @step. ^I (do not )?see outgoing (video )?call$
      */
-    @When("^I (do not )?see outgoing call$")
-    public void ISeeOutgoingCall(String not) throws Exception {
+    @When("^I (do not )?see outgoing (video )?call$")
+    public void ISeeOutgoingCall(String not, String videoCall) throws Exception {
+        boolean isVideoCall = videoCall != null;
         if (not == null) {
-            assertTrue("Outgoing call not visible", getPage().waitUntilVisible());
+            assertTrue("Outgoing call not visible", getPage().waitUntilVisible(isVideoCall));
         } else {
-            assertTrue("Outgoing call should not be visible", getPage().waitUntilNotVisible());
+            assertTrue("Outgoing call should not be visible", getPage().waitUntilNotVisible(isVideoCall));
         }
     }
     /**
