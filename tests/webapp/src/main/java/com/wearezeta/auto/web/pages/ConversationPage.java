23f2be54f082aa6deb272b7f2972da46c33a91a8
Sven Jost <sven@wire.com>
2016-05-09 15:44:57 +0200

    Webapp: QA-2020 Make File transfer form buttons invisible after transfer is completed

--- tests/webapp/src/main/java/com/wearezeta/auto/web/pages/ConversationPage.java
+++ tests/webapp/src/main/java/com/wearezeta/auto/web/pages/ConversationPage.java
@@ -409,6 +409,11 @@ public class ConversationPage extends WebPage {
 		getDriver().executeScript(showPathInputJScript);
 	}
 
+	public void moveCssSelectorOutOfViewport(String selector) throws Exception {
+		final String showPathInputJScript = "$(\"" + selector + "\").css({'left': 200});";
+		getDriver().executeScript(showPathInputJScript);
+	}
+
 	public void sendFile(String fileName) throws Exception {
 		final String filePath = WebCommonUtils.getFullFilePath("filetransfer/" + fileName);
 		hoverOverConversationInput();
@@ -418,6 +423,7 @@ public class ConversationPage extends WebPage {
 						this.getDriver(),
 						By.cssSelector(WebAppLocators.ConversationPage.cssSendFileInput));
 		filePathInput.sendKeys(filePath);
+		moveCssSelectorOutOfViewport(WebAppLocators.ConversationPage.cssSendFileInput);
 	}
 
 	public double getOverlapScoreOfLastImage(String pictureName)
