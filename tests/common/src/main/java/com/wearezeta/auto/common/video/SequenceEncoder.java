c71f75327c340fa310da64ddbfa13ee813e8e9cc
Robin <robin@wire.com>
2016-05-19 17:15:36 +0200

    Add comments for SequenceEncoder

--- tests/common/src/main/java/com/wearezeta/auto/common/video/SequenceEncoder.java
+++ tests/common/src/main/java/com/wearezeta/auto/common/video/SequenceEncoder.java
@@ -24,8 +24,8 @@ import org.jcodec.scale.ColorUtil;
 import org.jcodec.scale.Transform;
 
 /**
- * SequenceEncoder come from org.jcodec.api.awt.SequenceEncoder, but cannot use directly
- * based on transform issues, I copy it and update it
+ * SequenceEncoder come from org.jcodec.api.awt.SequenceEncoder,
+ * I copy it and update it to retrieve the channel size
  */
 public class SequenceEncoder {
     private SeekableByteChannel ch;
@@ -49,6 +49,13 @@ public class SequenceEncoder {
         this.ppsList = new ArrayList();
     }
 
+    /**
+     * Create one video frame from BufferedImage
+     *
+     * @param image
+     * @return the rendered video frame
+     * @throws IOException
+     */
     public Picture createFrameFromSingleImage(BufferedImage image) throws IOException {
         Picture pic = AWTUtil.fromBufferedImage(image);
         Picture renderedFrame = Picture.create(pic.getWidth(), pic.getHeight(), this.encoder.getSupportedColorSpaces()[0]);
@@ -56,6 +63,15 @@ public class SequenceEncoder {
         return renderedFrame;
     }
 
+    /**
+     * Please note, it doesn't support to add different size picuture into Picture.
+     * Such as first you add the picture 10 * 10, second time you cannot add the picture 100* 100
+     * You need to resize all pictures into same size
+     *
+     * @param picture
+     * @return current channel size
+     * @throws IOException
+     */
     public long addFrameToVideo(Picture picture) throws IOException {
         // each time it needs to init SPS and PPS in encoder.encodeFrame, so cannot extract it
         this._out.clear();
@@ -70,6 +86,12 @@ public class SequenceEncoder {
         return this.ch.size();
     }
 
+    /**
+     * Flush the data within channel into output file
+     * Should be called when you add all your frames
+     *
+     * @throws IOException
+     */
     public void finish() throws IOException {
         this.outTrack.addSampleEntry(H264Utils.createMOVSampleEntry(this.spsList, this.ppsList, 4));
         this.muxer.writeHeader();
