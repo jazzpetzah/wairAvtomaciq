6378e7bbc87457be3594be89b62774b924644596
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-05-27 13:29:57 +0200

    Add special run for permissions tests

---
+++ tests/android/src/test/java/com/wearezeta/auto/android/PermissionsRun.java
@@ -0,0 +1,12 @@
+package com.wearezeta.auto.android;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = {"html:target/report", "json:target/regression_report.json",
+        "com.wearezeta.auto.common.ZetaFormatter"}, tags = {"@v6Plus"})
+public class PermissionsRun {
+
+}
