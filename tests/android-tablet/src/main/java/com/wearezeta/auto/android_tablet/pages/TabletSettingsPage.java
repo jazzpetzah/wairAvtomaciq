07f11a0ece04b4f07237e59fdbb26582cc5a736f
Vova Bezhenar <vova@wire.com>
2016-06-29 18:54:19 +0300

    update methods

--- tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/TabletSettingsPage.java
+++ tests/android-tablet/src/main/java/com/wearezeta/auto/android_tablet/pages/TabletSettingsPage.java
@@ -7,7 +7,6 @@ import com.wearezeta.auto.android.pages.SettingsPage;
 import com.wearezeta.auto.common.driver.DriverUtils;
 import com.wearezeta.auto.common.driver.ZetaAndroidDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class TabletSettingsPage extends AndroidTabletPage {
 
@@ -18,29 +17,6 @@ public class TabletSettingsPage extends AndroidTabletPage {
     private SettingsPage getSettingsPage() throws Exception {
         return this.getAndroidPageInstance(SettingsPage.class);
     }
-
-    private static final String CURRENT_DEVICE = "Current Device";
-
-    private static final By xpathSettingsTitle = By.xpath("//*[@id='toolbar' and .//*[@value='Settings']]");
-
-    private static final Function<String, String> xpathStrSettingsMenuItemByText = text -> String
-            .format("//*[@id='title' and @value='%s']", text);
-
-    private static final Function<String, String> xpathStrConfirmBtnByName = name -> String
-            .format("//*[starts-with(@id, 'button') and @value='%s']", name);
-
-    private static final By xpathCurrentDevice = By.xpath(xpathStrSettingsMenuItemByText.apply(CURRENT_DEVICE)
-            + "/following::*[@id='title']");
-
-    private static final By idPasswordConfirmationInput = By.id("acet__remove_otr__password");
-
-    private static final By xpathConfirmationInputOKButton = By.xpath("//*[starts-with(@id, 'button') and @value='OK']");
-
-    private static final By idNameEdit = By.id("edit");
-
-    private static final By xpathOKButton = By.xpath("//*[starts-with(@id, 'button') and @value='OK']");
-
-    private static final By idEmailEdit = By.id("acet__preferences__email");
 
     private boolean scrollUntilMenuElementVisible(By locator, int maxScrolls) throws Exception {
         int nScrolls = 0;
@@ -55,56 +31,42 @@ public class TabletSettingsPage extends AndroidTabletPage {
     }
 
     public boolean waitUntilVisible() throws Exception {
-        return DriverUtils.waitUntilLocatorIsDisplayed(getDriver(), xpathSettingsTitle);
+        return getSettingsPage().waitUntilVisible();
     }
 
     public void selectMenuItem(String name) throws Exception {
-        final By locator = By.xpath(xpathStrSettingsMenuItemByText.apply(name));
-        assert scrollUntilMenuElementVisible(locator, 5) : String
-                .format("Menu item '%s' is not present", name);
-        getElement(locator).click();
+        getSettingsPage().selectMenuItem(name);
     }
 
     public void confirmLogout() throws Exception {
-        final By locator = By.xpath(xpathStrConfirmBtnByName.apply("Log out"));
-        getElement(locator, "Log out confirmation is not visible").click();
+        getSettingsPage().confirmLogout();
     }
 
     public void enterConfirmationPassword(String password) throws Exception {
-        final WebElement confirmationPasswordInput = getElement(idPasswordConfirmationInput);
-        confirmationPasswordInput.click();
-        confirmationPasswordInput.sendKeys(password);
+        getSettingsPage().enterConfirmationPassword(password);
     }
 
     public void tapOKButtonOnPasswordConfirmationDialog() throws Exception {
-        getElement(xpathConfirmationInputOKButton).click();
+        getSettingsPage().tapOKButtonOnPasswordConfirmationDialog();
     }
 
     public void tapCurrentDevice() throws Exception {
-        getElement(xpathCurrentDevice).click();
+        getSettingsPage().tapCurrentDevice();
     }
 
     public boolean waitUntilMenuItemVisible(String name) throws Exception {
-        final By locator = By.xpath(xpathStrSettingsMenuItemByText.apply(name));
-        return DriverUtils.waitUntilLocatorIsDisplayed(getDriver(), locator);
+        getSettingsPage().waitUntilVisible();
     }
 
     public boolean waitUntilMenuItemInvisible(String name) throws Exception {
-        final By locator = By.xpath(xpathStrSettingsMenuItemByText.apply(name));
-        return DriverUtils.waitUntilLocatorDissapears(getDriver(), locator);
+        getSettingsPage().waitUntilMenuItemInvisible(name);
     }
 
     public void commitNewName(String newName) throws Exception {
-        final WebElement nameEdit = getElement(idNameEdit);
-        nameEdit.clear();
-        nameEdit.sendKeys(newName);
-        getElement(xpathOKButton).click();
+        getSettingsPage().commitNewName(newName);
     }
 
     public void commitNewEmail(String newValue) throws Exception {
-        final WebElement emailEdit = getElement(idEmailEdit);
-        emailEdit.clear();
-        emailEdit.sendKeys(newValue);
-        getElement(xpathOKButton).click();
+        getSettingsPage().commitNewEmail(newValue);
     }
 }
