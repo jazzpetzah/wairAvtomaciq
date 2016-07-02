d2412012fe96f3de603351831f6f52856ff11363
Michael Koppen <michael.koppen@googlemail.com>
2016-07-02 15:03:44 +0200

    Webapp: Fixes gimport window problem

--- tests/webapp/src/main/java/com/wearezeta/auto/web/pages/external/GoogleLoginPage.java
+++ tests/webapp/src/main/java/com/wearezeta/auto/web/pages/external/GoogleLoginPage.java
@@ -4,10 +4,8 @@ import java.util.Set;
 import java.util.concurrent.Future;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 
 import com.wearezeta.auto.common.driver.DriverUtils;
 import com.wearezeta.auto.common.driver.ZetaWebAppDriver;
@@ -15,71 +13,73 @@ import com.wearezeta.auto.common.log.ZetaLogger;
 import com.wearezeta.auto.web.pages.WebPage;
 import java.util.concurrent.Callable;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 
 public class GoogleLoginPage extends WebPage {
-    
+
     private static final Logger log = ZetaLogger.getLog(GoogleLoginPage.class
-			.getSimpleName());
+            .getSimpleName());
 
-	// TODO move to Locators
-	private static final String EMAIL_ID = "Email";
-	private static final String PASSWORD_ID = "Passwd";
-	private static final String NEXT_BUTTON_ID = "next";
-	private static final String SIGN_IN_BUTTON_ID = "signIn";
-	private static final String SUBMIT_APPROVE_ACCESS_BUTTON_ID = "submit_approve_access";
+    // TODO move to Locators
+    private static final String EMAIL_ID = "Email";
+    private static final String PASSWORD_ID = "Passwd";
+    private static final String NEXT_BUTTON_ID = "next";
+    private static final String SIGN_IN_BUTTON_ID = "signIn";
+    private static final String SUBMIT_APPROVE_ACCESS_BUTTON_ID = "submit_approve_access";
 
-	@FindBy(id = EMAIL_ID)
-	private WebElement emailField;
+    @FindBy(id = EMAIL_ID)
+    private WebElement emailField;
 
-	@FindBy(id = PASSWORD_ID)
-	private WebElement passwordField;
+    @FindBy(id = PASSWORD_ID)
+    private WebElement passwordField;
 
-	@FindBy(id = NEXT_BUTTON_ID)
-	private WebElement nextButton;
+    @FindBy(id = NEXT_BUTTON_ID)
+    private WebElement nextButton;
 
-	@FindBy(id = SIGN_IN_BUTTON_ID)
-	private WebElement signInButton;
+    @FindBy(id = SIGN_IN_BUTTON_ID)
+    private WebElement signInButton;
 
-	@FindBy(id = SUBMIT_APPROVE_ACCESS_BUTTON_ID)
-	private WebElement approveAccessButton;
+    @FindBy(id = SUBMIT_APPROVE_ACCESS_BUTTON_ID)
+    private WebElement approveAccessButton;
 
-	public GoogleLoginPage(Future<ZetaWebAppDriver> lazyDriver)
-		throws Exception {
-		super(lazyDriver);
-	}
+    public GoogleLoginPage(Future<ZetaWebAppDriver> lazyDriver)
+            throws Exception {
+        super(lazyDriver);
+    }
 
-	public void setEmail(String email) throws Exception {
-		DriverUtils.waitUntilLocatorAppears(getDriver(), By.id(EMAIL_ID));
-		emailField.clear();
-		emailField.sendKeys(email);
-	}
+    public void setEmail(String email) throws Exception {
+        DriverUtils.waitUntilLocatorAppears(getDriver(), By.id(EMAIL_ID));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
 
-	public void setPassword(String password) throws Exception {
-		// this wait is needed when the NEXT button thing happens
-		DriverUtils.waitUntilLocatorAppears(getDriver(), By.id(PASSWORD_ID));
-		DriverUtils.waitUntilElementClickable(getDriver(), emailField);
-		passwordField.clear();
-		passwordField.sendKeys(password);
-	}
+    public void setPassword(String password) throws Exception {
+        // this wait is needed when the NEXT button thing happens
+        DriverUtils.waitUntilLocatorAppears(getDriver(), By.id(PASSWORD_ID));
+        DriverUtils.waitUntilElementClickable(getDriver(), emailField);
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
 
-	public boolean hasNextButton() throws Exception {
-		return this.getDriver().findElements(By.id(NEXT_BUTTON_ID)).size() > 0;
-	}
+    public boolean hasNextButton() throws Exception {
+        return this.getDriver().findElements(By.id(NEXT_BUTTON_ID)).size() > 0;
+    }
 
-	public void clickNext() throws Exception {
-		nextButton.click();
-	}
+    public void clickNext() throws Exception {
+        nextButton.click();
+    }
 
-	public boolean hasApproveButton() throws Exception {
-		return getDriver().findElements(By.id(SUBMIT_APPROVE_ACCESS_BUTTON_ID))
-			.size() > 0;
-	}
+    public boolean hasApproveButton() throws Exception {
+        return getDriver().findElements(By.id(SUBMIT_APPROVE_ACCESS_BUTTON_ID))
+                .size() > 0;
+    }
 
-	public void clickApprove() throws Exception {
-		assert DriverUtils.waitUntilElementClickable(getDriver(),
-			approveAccessButton) : "Can not click Approve button";
-		approveAccessButton.click();
-	}
+    public void clickApprove() throws Exception {
+        assert DriverUtils.waitUntilElementClickable(getDriver(),
+                approveAccessButton) : "Can not click Approve button";
+        approveAccessButton.click();
+    }
 
     public void clickSignIn() throws Exception {
         signInButton.click();
@@ -97,16 +97,27 @@ public class GoogleLoginPage extends WebPage {
 
         enclosedStep.call();
 
+        /**
+         * We can not wait for the gimport window to close frist because once we get a UnreachableBrowserException the driver is
+         * dead. Thus we have to switch the window before the gimport window is actually closed and check if it's closed
+         * afterwards. The UnreachableBrowserException itself seems to be a bug that you can't get the WindowHandles after the
+         * currently selected window within the driver is closing itself.
+         */
+        log.debug("before windowSwitch");
+        this.getDriver().switchTo().window(handles.stream().findFirst().get());
+        log.debug("after windowSwitch");
         // wait for popup to close		
         this.getWait().until(new ExpectedCondition<Boolean>() {
             @Override
             public Boolean apply(WebDriver input) {
-                return input.getWindowHandles().size() < handles.size();
+                try {
+                    return input.getWindowHandles().size() < handles.size();
+                } catch (Exception e) {
+                    return true;
+                }
             }
         });
-        Thread.sleep(2000);
-        // switch back to main window
-        this.getDriver().switchTo().window(this.getDriver().getWindowHandles().iterator().next());
+        log.debug("after close check");
     }
 
 }
