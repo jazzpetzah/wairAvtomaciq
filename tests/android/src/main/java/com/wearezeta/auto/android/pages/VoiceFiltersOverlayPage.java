d8db2a6ca7aa615ceaa97f5a99fda8c0d9390506
Robin <robin@wire.com>
2016-07-05 15:01:58 +0200

    QA-2236

--- tests/android/src/main/java/com/wearezeta/auto/android/pages/VoiceFiltersOverlayPage.java
+++ tests/android/src/main/java/com/wearezeta/auto/android/pages/VoiceFiltersOverlayPage.java
@@ -5,12 +5,18 @@ import com.wearezeta.auto.common.driver.ZetaAndroidDriver;
 import org.openqa.selenium.By;
 
 import java.util.concurrent.Future;
+import java.util.function.Function;
 
 public class VoiceFiltersOverlayPage extends AndroidPage {
 
     private static final By idVoiceRecordingContainer = By.id("ttv__voice_filter__tap_to_record");
     private static final By idRecordButton = By.id("gtv__record_button");
     private static final By idApproveButton = By.id("v__voice_approve");
+    private static final By idVoiceGraph = By.id("wbv__voice_filter");
+
+    private static final Function<Integer, String> xpathFilterItemButton = index -> String
+            .format("(//*[@id='gtv__voice_filter_icon'])[%d]", index);
+
 
     public VoiceFiltersOverlayPage(Future<ZetaAndroidDriver> lazyDriver) throws Exception {
         super(lazyDriver);
@@ -21,8 +27,17 @@ public class VoiceFiltersOverlayPage extends AndroidPage {
         getElement(locator).click();
     }
 
+    public void tapFilterButton(int index) throws Exception {
+        By locator = By.xpath(xpathFilterItemButton.apply(index));
+        getDriver().longTap(getElement(locator), DriverUtils.SINGLE_TAP_DURATION);
+    }
+
     public boolean isVoiceRecordingDialogVisible() throws Exception {
         return DriverUtils.waitUntilLocatorIsDisplayed(getDriver(), idVoiceRecordingContainer);
+    }
+
+    public boolean isVoiceGraphVisible() throws Exception {
+        return DriverUtils.waitUntilLocatorIsDisplayed(getDriver(), idVoiceGraph);
     }
 
     private By getButtonLocatorByName(String name) {
--- tests/android/src/test/java/com/wearezeta/auto/android/steps/VoiceFiltersOverlayPageSteps.java
+++ tests/android/src/test/java/com/wearezeta/auto/android/steps/VoiceFiltersOverlayPageSteps.java
@@ -17,11 +17,16 @@ public class VoiceFiltersOverlayPageSteps {
      * Tap on buttons on voice filters overlay
      *
      * @param buttonName the button name within voice filters overlay
-     * @step. ^I tap (Start Record|Stop Record|Approve) button on Voice filters overlay$
+     * @step. ^I tap (Start Record|Stop Record|Approve|the \d+(?:st|nd|rd|th) button on Voice filters overlay$
      */
-    @When("^I tap (Start Record|Stop Record|Approve) button on Voice filters overlay$")
+    @When("^I tap (Start Record|Stop Record|Approve|the \\d+(?:st|nd|rd|th) Filter) button on Voice filters overlay$")
     public void ITapOnVoiceFiltersOverlayButton(String buttonName) throws Exception {
-        getVoiceFiltersOverlayPage().tapButton(buttonName);
+        if (buttonName.startsWith("the")) {
+            int index = Integer.parseInt(buttonName.replaceAll("[\\D]", ""));
+            getVoiceFiltersOverlayPage().tapFilterButton(index);
+        } else {
+            getVoiceFiltersOverlayPage().tapButton(buttonName);
+        }
     }
 
     /**
@@ -33,5 +38,16 @@ public class VoiceFiltersOverlayPageSteps {
     @Then("^I see Voice filters overlay$")
     public void ISeeVoiceRecordingDialog() throws Exception {
         Assert.assertTrue("The voice recording dialog should be visible", getVoiceFiltersOverlayPage().isVoiceRecordingDialogVisible());
+    }
+
+    /**
+     * Verify the voice graph is visible
+     *
+     * @throws Exception
+     * @step. ^I see voice graph on Voice filters overlay$
+     */
+    @Then("^I see voice graph on Voice filters overlay$")
+    public void ISeeRecordGraph() throws Exception {
+        Assert.assertTrue("The voice graph should be visible", getVoiceFiltersOverlayPage().isVoiceGraphVisible());
     }
 }
