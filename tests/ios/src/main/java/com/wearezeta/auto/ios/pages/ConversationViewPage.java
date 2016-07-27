eb7814b641283a334410e6eb8f9d0bc85663cb91
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-07-27 16:42:29 +0200

    More optimisations to typeMessage method

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationViewPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationViewPage.java
@@ -14,6 +14,7 @@ import com.wearezeta.auto.common.sync_engine_bridge.Constants;
 import com.wearezeta.auto.ios.tools.IOSSimulatorHelper;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.ios.IOSElement;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
@@ -463,15 +464,17 @@ public class ConversationViewPage extends IOSPage {
     public void typeMessage(String message, boolean shouldSend) throws Exception {
         final WebElement convoInput = getElement(nameConversationInput,
                 "Conversation input is not visible after the timeout");
-        final boolean isKbdInvisible = this.isKeyboardInvisible(1);
-        convoInput.click();
-        if (isKbdInvisible) {
+        if (this.isKeyboardInvisible(1)) {
+            convoInput.click();
             // Wait for keyboard opening animation
             Thread.sleep(KEYBOARD_OPEN_ANIMATION_DURATION);
         }
-        convoInput.sendKeys(message);
         if (shouldSend) {
+            // This is faster and allows to avoid autocorrection, but does not update input cursor position properly
+            ((IOSElement) convoInput).setValue(message);
             this.tapKeyboardCommitButton();
+        } else {
+            convoInput.sendKeys(message);
         }
     }
 
