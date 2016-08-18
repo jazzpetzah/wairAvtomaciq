11f058650579c484bc84e84805475ea9f9403d67
Piotr <piotr@wire.com>
2016-08-18 16:26:23 +0300

    remove not necessary code

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/SketchPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/SketchPage.java
@@ -4,10 +4,8 @@ import java.util.Random;
 import java.util.concurrent.Future;
 
 import com.wearezeta.auto.common.CommonUtils;
-import com.wearezeta.auto.common.log.ZetaLogger;
 import com.wearezeta.auto.ios.tools.IOSSimulatorHelper;
 import io.appium.java_client.MobileBy;
-import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 
 import com.wearezeta.auto.common.driver.ZetaIOSDriver;
@@ -15,7 +13,6 @@ import com.wearezeta.auto.common.driver.DriverUtils;
 
 public class SketchPage extends IOSPage {
 
-    private static final Logger log = ZetaLogger.getLog(SketchPage.class.getSimpleName());
     private static final By nameSketchSendButton = MobileBy.AccessibilityId("SketchConfirmButton");
     private static final By xpathTapColorLabel = By.xpath("//UIAStaticText[contains(@name, 'TAP COLOR')]");
 
@@ -34,8 +31,6 @@ public class SketchPage extends IOSPage {
             if (CommonUtils.getIsSimulatorFromConfig(getClass())) {
                 IOSSimulatorHelper.swipe(startX / 100.0, startY / 100.0, endX / 100.0, endY / 100.0);
             } else {
-                log.debug(getDriver().manage().window().getSize());
-                log.debug(startX + "/" + startY + "/" + endX + "/" + endY);
                 DriverUtils.swipeByCoordinates(getDriver(), 0, startX, startY, endX, endY);
             }
         }
