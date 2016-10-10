a6ce4987970fae2baed3f9e625b8d8582f81bfbc
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-10-10 11:03:02 +0200

    Remove unused locator

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/OtherUserPersonalInfoPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/OtherUserPersonalInfoPage.java
@@ -55,7 +55,6 @@ public class OtherUserPersonalInfoPage extends IOSPage {
     private static final String xpathStrDevicesList = "//XCUIElementTypeTable/XCUIElementTypeCell";
     private static final Function<Integer, String> xpathStrDevicesByCount = count ->
             String.format("//XCUIElementTypeTable[count(XCUIElementTypeCell)=%s]", count);
-    private static final By xpathDevicesList = By.xpath(xpathStrDevicesList);
     private static final Function<Integer, String> xpathStrDeviceByIndex =
             idx -> String.format("%s[%s]", xpathStrDevicesList, idx);
 
