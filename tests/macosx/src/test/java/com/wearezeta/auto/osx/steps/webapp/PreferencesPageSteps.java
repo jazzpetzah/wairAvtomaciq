9ac9e99d5697fbf323650117f1302972dc853c00
Michael Koppen <michael.koppen@googlemail.com>
2016-10-13 15:54:47 +0200

    Webapp: Fixes compile errors for MacOS Wrapper Webapp: Moves steps from settings to device page Webapp: Removes settings page/steps

--- tests/macosx/src/test/java/com/wearezeta/auto/osx/steps/webapp/PreferencesPageSteps.java
+++ tests/macosx/src/test/java/com/wearezeta/auto/osx/steps/webapp/PreferencesPageSteps.java
@@ -1,46 +1,44 @@
 package com.wearezeta.auto.osx.steps.webapp;
 
-import com.wearezeta.auto.osx.pages.webapp.SettingsPage;
-import org.junit.Assert;
-
-import com.wearezeta.auto.web.pages.WebappPagesCollection;
-
-import cucumber.api.java.en.Then;
+import com.wearezeta.auto.web.common.TestContext;
+import com.wearezeta.auto.web.pages.PreferencesPage;
 import cucumber.api.java.en.When;
 
 public class PreferencesPageSteps {
 
-    /**
-     * Verifies whether preferences dialog is visible
-     *
-     * @step. ^I see [Pp]references dialog$
-     *
-     * @throws AssertionError if preferences dialog is not currently visible
-     */
-    @Then("^I see [Pp]references dialog$")
-    public void ISeeSetingsDialog() throws Exception {
-        Assert.assertTrue(WebappPagesCollection.getInstance()
-                .getPage(SettingsPage.class).isVisible());
+    private final TestContext context;
+
+    public PreferencesPageSteps() {
+        this.context = new TestContext();
+    }
+
+    public PreferencesPageSteps(TestContext context) {
+        this.context = context;
+    }
+
+    @When("^I close preferences$")
+    public void IClosePreferences() throws Exception {
+        context.getPagesCollection().getPage(PreferencesPage.class).closePreferences();
+    }
+
+    @When("^I open account in preferences$")
+    public void IOpenAccount() throws Exception {
+        context.getPagesCollection().getPage(PreferencesPage.class).openAccount();
     }
     
-    /**
-     * Click on import contacts from Gmail via Setting
-     *
-     * @throws Exception
-     */
-    @When("^I click button to import contacts from Gmail$")
-    public void IClickImportButton() throws Exception {
-        WebappPagesCollection.getInstance().getPage(SettingsPage.class).clickImportButton();
+    @When("^I open devices in preferences$")
+    public void IOpenDevices() throws Exception {
+        context.getPagesCollection().getPage(PreferencesPage.class).openDevices();
     }
     
-    /**
-     * Click on import contacts from address book via Setting
-     *
-     * @throws Exception
-     */
-    @When("^I click button to import contacts from address book via preferences$")
-    public void IClickAddressbookImportButton() throws Exception {
-        WebappPagesCollection.getInstance().getPage(SettingsPage.class).clickImportAddressbookButton();
+    @When("^I open options in preferences$")
+    public void IOpenOptions() throws Exception {
+        context.getPagesCollection().getPage(PreferencesPage.class).openOptions();
+    }
+    
+    @When("^I open about in preferences$")
+    public void IOpenAbout() throws Exception {
+        context.getPagesCollection().getPage(PreferencesPage.class).openAbout();
     }
 
 }
