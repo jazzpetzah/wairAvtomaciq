df22fb423874621b110ab0b47b8cbf7379734a0e
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-10-20 12:09:07 +0200

    Remove unnecessary workarounds

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationViewPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationViewPage.java
@@ -775,9 +775,6 @@ public class ConversationViewPage extends IOSPage {
     }
 
     public void tapRecordControlButton(String buttonName) throws Exception {
-        //sometimes for such dynamic elements like record bar appium do not get the actual page source
-        //in some cases this method helps to refresh elements tree.
-        this.printPageSource();
         By button = getRecordControlButtonByName(buttonName);
         if (button.equals(namePlayAudioRecorderButton)) {
             getElement(button).click();
