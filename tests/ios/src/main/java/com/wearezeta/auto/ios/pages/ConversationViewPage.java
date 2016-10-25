b88697fb6a13cbe8e6a1a5d8036d2e55bc86040d
Mykola Mokhnach <mykola.mokhnach@wearezeta.com>
2016-10-25 10:14:07 +0200

    Deprecate workarounds for clicks on text messages

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationViewPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationViewPage.java
@@ -714,17 +714,12 @@ public class ConversationViewPage extends IOSPage {
 
     public void tapMessageByText(boolean isLongTap, boolean isDoubleTap, String msg) throws Exception {
         final FBElement el = (FBElement) getElement(FBBy.xpath(xpathStrMessageByTextPart.apply(msg)));
-        // The tap should be performed precisely on the text
-        final int tapPercentX = 10;
-        final int tapPercentY = 50;
         if (isDoubleTap) {
-            doubleClickAt(el, tapPercentX, tapPercentY);
+            el.doubleTap();
+        } else if (isLongTap) {
+            el.longTap();
         } else {
-            if (isLongTap) {
-                longClickAt(el, tapPercentX, tapPercentY);
-            } else {
-                this.tapByPercentOfElementSize(el, tapPercentX, tapPercentY);
-            }
+            el.click();
         }
     }
 
