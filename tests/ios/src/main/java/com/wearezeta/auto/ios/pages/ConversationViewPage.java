f35c407cde70f13c40fcc724bfa71e6d13472237
Juliane Reschke <juliane@wire.com>
2016-10-26 15:50:04 +0200

    iOS: need the methods back for just media container compare for the soundcloud play button

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationViewPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationViewPage.java
@@ -501,23 +501,23 @@ public class ConversationViewPage extends IOSPage {
         getElement(nameRecentMessageToolbox).click();
     }
 
-//    public BufferedImage getMediaContainerStateGlyphScreenshot() throws Exception {
-//        final BufferedImage containerScreen =
-//                this.getElementScreenshot(getElement(fbXpathMediaContainerCell)).orElseThrow(() ->
-//                        new IllegalStateException("Cannot take a screenshot of media container"));
-//        final int stateGlyphWidth = containerScreen.getWidth() / 7;
-//        final int stateGlyphHeight = containerScreen.getHeight() / 7;
-//        final int stateGlyphX = (containerScreen.getWidth() - stateGlyphWidth) / 2;
-//        final int stateGlyphY = (containerScreen.getHeight() - stateGlyphHeight) / 2;
+    public BufferedImage getMediaContainerStateGlyphScreenshot() throws Exception {
+        final BufferedImage containerScreen =
+                this.getElementScreenshot(getElement(fbXpathMediaContainerCell)).orElseThrow(() ->
+                        new IllegalStateException("Cannot take a screenshot of media container"));
+        final int stateGlyphWidth = containerScreen.getWidth() / 7;
+        final int stateGlyphHeight = containerScreen.getHeight() / 7;
+        final int stateGlyphX = (containerScreen.getWidth() - stateGlyphWidth) / 2;
+        final int stateGlyphY = (containerScreen.getHeight() - stateGlyphHeight) / 2;
 //        BufferedImage tmp = containerScreen.getSubimage(stateGlyphX, stateGlyphY, stateGlyphWidth, stateGlyphHeight);
 //        ImageIO.write(tmp, "png", new File("/Users/julianereschke/Desktop/" + System.currentTimeMillis() + ".png"));
-//        return containerScreen.getSubimage(stateGlyphX, stateGlyphY, stateGlyphWidth, stateGlyphHeight);
-//    }
+        return containerScreen.getSubimage(stateGlyphX, stateGlyphY, stateGlyphWidth, stateGlyphHeight);
+    }
 
     public BufferedImage getAssetsContainerStateScreenshot() throws Exception {
         final BufferedImage containerScreen = this.getElementScreenshot(getElement(xpathSecondEntry)).orElseThrow(() ->
-                new IllegalStateException("Cannot take a screenshot of File Share container"));
-        ImageIO.write(containerScreen, "png", new File("/Users/julianereschke/Desktop/" + System.currentTimeMillis() + ".png"));
+                new IllegalStateException("Cannot take a screenshot of asset container"));
+//        ImageIO.write(containerScreen, "png", new File("/Users/julianereschke/Desktop/" + System.currentTimeMillis() + ".png"));
         return containerScreen;
     }
 
