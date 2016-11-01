d68e1a36626f4851499daf7e748368eba6745a79
Vova Bezhenar <vbezhenar@git.lohika.com>
2016-11-01 12:09:16 +0200

    pressing > typing

--- tests/android/src/test/java/com/wearezeta/auto/android/steps/RegistrationSteps.java
+++ tests/android/src/test/java/com/wearezeta/auto/android/steps/RegistrationSteps.java
@@ -40,7 +40,7 @@ public class RegistrationSteps {
     }
 
     /**
-     * Submits all of the data given in the registration process by pressing the
+     * Submits all of the data given in the registration process by taping the
      * "create account" button Also waits at the inbox of the user to check to
      * receive the verification email
      *
