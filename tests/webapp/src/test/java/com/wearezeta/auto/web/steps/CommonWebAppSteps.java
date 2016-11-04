27e8d5ce08cf6414e3783dfe35bc20094e56e3f7
Sven Jost <sven@wire.com>
2016-11-04 17:02:08 +0100

    Revert "Webapp eph group (#1438)"
    This reverts commit 932e236eed285faba736b38e0659f747a09f911a.

--- tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/steps/CommonWebAppSteps.java
@@ -15,6 +15,7 @@ import com.wearezeta.auto.web.common.Message;
 import com.wearezeta.auto.web.common.TestContext;
 import com.wearezeta.auto.web.common.WebAppExecutionContext;
 import com.wearezeta.auto.web.common.WebCommonUtils;
+import com.wearezeta.auto.web.pages.ConversationPage;
 import com.wearezeta.auto.web.pages.RegistrationPage;
 import com.wearezeta.auto.web.pages.WebPage;
 import com.wearezeta.auto.web.pages.external.DeleteAccountPage;
@@ -429,7 +430,7 @@ public class CommonWebAppSteps {
         if (convoType.equals("user")) {
             context.getCommonSteps().UserSentOtrMessageToUser(msgFromUserNameAlias, dstConvoName, msgToSend, deviceName + context.getTestname().hashCode());
         } else {
-            context.getCommonSteps().UserSentOtrMessageToGroupConversation(msgFromUserNameAlias, dstConvoName, msgToSend, deviceName + context.getTestname().hashCode());
+            context.getCommonSteps().UserSentOtrMessageToConversation(msgFromUserNameAlias, dstConvoName, msgToSend, deviceName + context.getTestname().hashCode());
         }
     }
 
@@ -446,7 +447,7 @@ public class CommonWebAppSteps {
         } else {
             for (int i = 0; i < amount; i++) {
                 context.getConversationStates().addMessage(dstConvoName, new Message(prefix + i, user.getId()));
-                context.getCommonSteps().UserSentOtrMessageToGroupConversation(msgFromUserNameAlias, dstConvoName, prefix + i,
+                context.getCommonSteps().UserSentOtrMessageToConversation(msgFromUserNameAlias, dstConvoName, prefix + i,
                         deviceName + context.getTestname().hashCode());
             }
         }
@@ -460,7 +461,7 @@ public class CommonWebAppSteps {
             context.getCommonSteps().UserSentOtrMessageToUser(msgFromUserNameAlias, dstConvoName, message, deviceName + context.
                     getTestname().hashCode());
         } else {
-            context.getCommonSteps().UserSentOtrMessageToGroupConversation(msgFromUserNameAlias, dstConvoName, message,
+            context.getCommonSteps().UserSentOtrMessageToConversation(msgFromUserNameAlias, dstConvoName, message,
                     deviceName + context.getTestname().hashCode());
         }
     }
