6df20a469710f9b1a6355eb4c7ca333367a454d8
Michael Koppen <michael.koppen@googlemail.com>
2016-11-14 15:56:22 +0100

    Wrapper macOS: Bring back step "I verify active conversation is at index X"

--- tests/macosx/src/test/java/com/wearezeta/auto/osx/steps/webapp/ContactListPageSteps.java
+++ tests/macosx/src/test/java/com/wearezeta/auto/osx/steps/webapp/ContactListPageSteps.java
@@ -4,6 +4,9 @@ package com.wearezeta.auto.osx.steps.webapp;
 import org.apache.log4j.Logger;
 import com.wearezeta.auto.common.log.ZetaLogger;
 import com.wearezeta.auto.web.common.TestContext;
+import com.wearezeta.auto.web.pages.ContactListPage;
+import cucumber.api.java.en.Then;
+import org.junit.Assert;
 
 public class ContactListPageSteps {
 
@@ -15,6 +18,15 @@ public class ContactListPageSteps {
     public ContactListPageSteps(TestContext webContext, TestContext wrapperContext) {
         this.webContext = webContext;
         this.wrapperContext = wrapperContext;
+    }
+    
+    //TODO move to webapp
+    @Then("^I verify active conversation is at index (\\d+)$")
+    public void IVerifyActiveConversationIsAtIndex(int expectedIndex) throws Exception {
+        final int actualIndex = webContext.getPagesCollection().getPage(ContactListPage.class).getActiveConversationIndex();
+        Assert.assertTrue(String.format(
+                        "The index of active item in Conversations list does not equal to %s (current value is %s)",
+                        expectedIndex, actualIndex), actualIndex == expectedIndex);
     }
 
 }
