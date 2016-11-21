56cc57183dc78f30287a56ca29394022f6a89b1b
Mykola Mokhnach <nick+1@wire.com>
2016-11-21 09:38:19 +0100

    Need to wait longer for the animation

--- tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationActionsPage.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/ConversationActionsPage.java
@@ -48,7 +48,7 @@ public class ConversationActionsPage extends IOSPage {
 
     public void confirmAction(String actionName) throws Exception {
         // Wait for animation
-        Thread.sleep(1500);
+        Thread.sleep(2500);
         By locator = FBBy.xpath(xpathStrConfirmActionButtonByName.apply(actionName));
         switch (actionName.toLowerCase()) {
             case "cancel request":
