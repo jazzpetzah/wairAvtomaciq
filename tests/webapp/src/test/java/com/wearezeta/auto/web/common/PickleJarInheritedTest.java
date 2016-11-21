b721310b8d13cbc3720ad9dfc61075ec4a2ba700
Anastasia <anastasiia.gerasymenko@wearezeta.com>
2016-11-21 17:27:22 +0100

    Webapp: added youtube javascript error to the browser log exception in testTemplate.txt and to PickleJarInheritedTest.java

--- tests/webapp/src/test/java/com/wearezeta/auto/web/common/PickleJarInheritedTest.java
+++ tests/webapp/src/test/java/com/wearezeta/auto/web/common/PickleJarInheritedTest.java
@@ -219,10 +219,10 @@ public class PickleJarInheritedTest extends PickleJarTest {
                     // filter encryption precondition
                     .filter((entry)
                             -> !entry.getMessage().contains("otr") && !entry.getMessage().contains("412 (Precondition Failed)"))
-                    .collect(Collectors.toList())
                     //filter youtube javascript error
                     .filter((entry)
-                             -> !entry.getMessage().contains("/access") && !entry.getMessage().contains("403"));
+                             -> !entry.getMessage().contains("ytcfg is not defined"))
+                    .collect(Collectors.toList());
 
         } catch (Exception e) {
             LOG.warn("No error log check available", e);
