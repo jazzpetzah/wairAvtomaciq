c10a283e22e9a5dd8eada2cbe0c874020dc4b976
Vova Bezhenar <vbezhenar@git.lohika.com>
2016-11-28 13:32:48 +0200

    formatting

--- tests/android/src/main/java/com/wearezeta/auto/android/pages/OtherUserPersonalInfoPage.java
+++ tests/android/src/main/java/com/wearezeta/auto/android/pages/OtherUserPersonalInfoPage.java
@@ -221,8 +221,8 @@ public class OtherUserPersonalInfoPage extends AndroidPage {
         return DriverUtils.waitUntilLocatorIsDisplayed(getDriver(),
                 By.xpath(xpathParticipantEmailByText.apply(expectedEmail)), 1)
                 || DriverUtils.waitUntilLocatorIsDisplayed(getDriver(),
-                By.xpath(xpathSingleParticipantEmailByText.apply(expectedEmail)), 1) ||
-                DriverUtils.waitUntilLocatorIsDisplayed(getDriver(),
+                By.xpath(xpathSingleParticipantEmailByText.apply(expectedEmail)), 1)
+                || DriverUtils.waitUntilLocatorIsDisplayed(getDriver(),
                 By.xpath(xpathParticipantEmailByText.apply("@whisker_pants")), 1)
                 || DriverUtils.waitUntilLocatorIsDisplayed(getDriver(),
                 By.xpath(xpathSingleParticipantEmailByText.apply("@whisker_pants")), 1);
