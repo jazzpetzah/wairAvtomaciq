e98ae2132753eb662b9349048a0a3a0483f1c9a8
Juliane Reschke <juliane@wire.com>
2016-12-21 13:05:02 +0100

    iOS: initial Collection test

---
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/pages/CollectionPage.java
@@ -0,0 +1,7 @@
+package com.wearezeta.auto.ios.pages;
+
+/**
+ * Created by julianereschke on 21/12/16.
+ */
+public class CollectionPage {
+}
---
+++ tests/ios/src/test/java/com/wearezeta/auto/ios/steps/CollectionPageSteps.java
@@ -0,0 +1,7 @@
+package com.wearezeta.auto.ios.steps;
+
+/**
+ * Created by julianereschke on 21/12/16.
+ */
+public class CollectionPageSteps {
+}
