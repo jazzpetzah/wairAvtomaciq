147d2009bbf4d975d472165e974ee162b1443276
Mykola Mokhnach <nick+1@wire.com>
2016-12-23 16:10:27 +0100

    improve events search algorithm

--- tests/ios/src/main/java/com/wearezeta/auto/ios/tools/LocalyticsHelpers.java
+++ tests/ios/src/main/java/com/wearezeta/auto/ios/tools/LocalyticsHelpers.java
@@ -1,33 +1,65 @@
 package com.wearezeta.auto.ios.tools;
 
-import java.util.Arrays;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 public class LocalyticsHelpers {
-    private static final String LOG_RECORD_MARKER = "<ANALYTICS>";
+    private static final String LOG_RECORD_MARKER = "<ANALYTICS>:";
 
-    private static List<String> filterLog(String fullLog) {
-        return Arrays.stream(fullLog.split("\n")).filter(x ->
-                x.contains(LOG_RECORD_MARKER)
-        ).collect(Collectors.toList());
+    private static List<JSONObject> filterLog(String fullLog) {
+        final List<JSONObject> result = new ArrayList<>();
+        boolean isRecordStarted = false;
+        StringBuilder currentRecord = new StringBuilder();
+        for (final String logLine : fullLog.split("\n")) {
+            final int matchPos = logLine.indexOf(LOG_RECORD_MARKER);
+            if (matchPos > 0) {
+                isRecordStarted = true;
+                currentRecord = new StringBuilder();
+                currentRecord.append(logLine.substring(matchPos + LOG_RECORD_MARKER.length()));
+                continue;
+            }
+            if (isRecordStarted && logLine.startsWith(" ")) {
+                currentRecord.append(logLine);
+                continue;
+            }
+            final String resultRecord = currentRecord.toString();
+            if (resultRecord.length() > 0) {
+                result.add(new JSONObject(resultRecord.trim()));
+            }
+            isRecordStarted = false;
+        }
+        return result;
     }
 
-    private static final Pattern EVENT_LOG_PATTERN = Pattern.compile(
-            "Tagging Event:\\s+((?:\\S|\\s(?!\\s))*)"
-    );
+    private static final String EVENT_NAME_KEY = "event";
+    private static final String ATTRIBUTES_NAME_KEY = "attributes";
 
-    public static int getEventOccurrencesCount(String currentLog, String eventName) {
-        final List<String> allRecords = filterLog(currentLog);
-        int occurrences = 0;
-        for (String logRecord : allRecords) {
-            final Matcher matcher = EVENT_LOG_PATTERN.matcher(logRecord);
-            if (matcher.find() && matcher.group(1).equals(eventName)) {
-                ++occurrences;
+    public static long getEventOccurrencesCount(String currentLog, String eventName) {
+        return filterLog(currentLog).stream().filter(
+                x -> x.has(EVENT_NAME_KEY) && x.getString(EVENT_NAME_KEY).equals(eventName)
+        ).count();
+    }
+
+    private static boolean hasAllMapItems(JSONObject src, Map<String, Object> expectedAttributes) {
+        for (Map.Entry<String, Object> entry : expectedAttributes.entrySet()) {
+            final String attrName = entry.getKey();
+            final Object attrValue = entry.getValue();
+            if (!src.has(attrName) || !src.get(attrName).equals(attrValue)) {
+                return false;
             }
         }
-        return occurrences;
+        return true;
+    }
+
+    public static long getEventOccurrencesCount(String currentLog, String eventName,
+                                                Map<String, Object> expectedAttributes) {
+        return filterLog(currentLog).stream().filter(
+                x -> x.has(EVENT_NAME_KEY) && x.getString(EVENT_NAME_KEY).equals(eventName) &&
+                        x.has(ATTRIBUTES_NAME_KEY) && hasAllMapItems(x.getJSONObject(ATTRIBUTES_NAME_KEY),
+                        expectedAttributes)
+        ).count();
     }
 }
