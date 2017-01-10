02dbfbea354e1ee61e277a036b38c5cffe76e5c8
Michael Koppen <michael.koppen@googlemail.com>
2017-01-10 16:44:48 +0100

    Common: Adds calling v3 calling service webapp environments

--- tests/common/src/main/java/com/wearezeta/auto/common/calling2/v1/model/BackendType.java
+++ tests/common/src/main/java/com/wearezeta/auto/common/calling2/v1/model/BackendType.java
@@ -2,5 +2,6 @@ package com.wearezeta.auto.common.calling2.v1.model;
 
 public enum BackendType {
 
-	DEV, EDGE, STAGING, PRODUCTION, BENNY, GREGOR, RENE, LIPIS, CHRIS;
+	DEV, EDGE, STAGING, PRODUCTION, BENNY, GREGOR, RENE, LIPIS, CHRIS, 
+        DEV_CV3, EDGE_CV3, STAGING_CV3, GREGOR_CV3;
 }
